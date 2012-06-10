9c223e9fd5b081182c00cbff808c47692f030df4
davboecki <dboecken@yahoo.de>
2012-06-10 13:31:37 +0200

    Clean up

--- server/net/minecraft/src/buildcraft/logisticspipes/modules/IModuleGuiHandler.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.src.buildcraft.logisticspipes.modules;
-
-import net.minecraft.src.EntityPlayer;
-
-public interface IModuleGuiHandler {
-
-	boolean displayGui(EntityPlayer entityplayer,ILogisticsModule logisticsModule, Object object);}

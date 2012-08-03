e6b435155833d6df67f799f6f1398812af33a0be
davboecki <dboeken@yahoo.de>
2012-08-03 20:44:47 +0200

    Fix dummy class

--- dummy/codechicken/nei/MultiItemRange.java
+++ dummy/codechicken/nei/MultiItemRange.java
@@ -1,8 +1,40 @@
 package codechicken.nei;
 
+import java.util.Collection;
+
+import net.minecraft.src.Block;
 import net.minecraft.src.Item;
+import net.minecraft.src.ItemStack;
 
 public class MultiItemRange {
-	public void add(Item logisticsCraftingSignCreator) {}
-	public void add(Item moduleItem, int i, int j) {}
+
+	public MultiItemRange add(Collection<?> ranges)
+	{return null;}
+	
+	public MultiItemRange add(MultiItemRange range)
+	{return null;}
+	
+	public MultiItemRange add(int itemID)
+	{return null;}
+	
+	public MultiItemRange add(int itemID, int damageStart, int damageEnd)
+	{return null;}
+	
+	public MultiItemRange add(int itemIDFirst, int itemIDLast)
+	{return null;}
+
+	public MultiItemRange add(Item item, int damageStart, int damageEnd)
+	{return null;}
+	
+	public MultiItemRange add(Block block, int damageStart, int damageEnd)
+	{return null;}
+	
+	public MultiItemRange add(Item item)
+	{return null;}
+	
+	public MultiItemRange add(Block block)
+	{return null;}
+	
+	public MultiItemRange add(ItemStack item)
+	{return null;}
 }

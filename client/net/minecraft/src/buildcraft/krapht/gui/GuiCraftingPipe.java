f86de785a64c892080ebbaaee93434afa3d3f1d8
davboecki <dboeken@yahoo.de>
2012-08-07 00:08:03 +0200

    Added warning to Crafting Gui for non stackable items and created auto move button. Addresses #28

--- client/net/minecraft/src/buildcraft/krapht/gui/GuiCraftingPipe.java
+++ client/net/minecraft/src/buildcraft/krapht/gui/GuiCraftingPipe.java
@@ -13,6 +13,7 @@ import net.minecraft.src.GuiButton;
 import net.minecraft.src.GuiContainer;
 import net.minecraft.src.IInventory;
 import net.minecraft.src.InventoryPlayer;
+import net.minecraft.src.Slot;
 import net.minecraft.src.core_LogisticsPipes;
 import net.minecraft.src.buildcraft.krapht.GuiIDs;
 import net.minecraft.src.buildcraft.krapht.logic.LogicCrafting;
@@ -27,6 +28,7 @@ public class GuiCraftingPipe extends GuiContainer implements IGuiIDHandlerProvid
 
 	private final LogicCrafting _logic;
 	private final EntityPlayer _player;
+	private final GuiButton[] buttonarray;
 	
 	public GuiCraftingPipe(EntityPlayer player, IInventory dummyInventory, LogicCrafting logic) {
 		super(null);
@@ -46,6 +48,7 @@ public class GuiCraftingPipe extends GuiContainer implements IGuiIDHandlerProvid
 		_logic = logic;
 		xSize = 195;
 		ySize = 187;
+		buttonarray = new GuiButton[6];
 	}
 
 	@Override
@@ -57,11 +60,18 @@ public class GuiCraftingPipe extends GuiContainer implements IGuiIDHandlerProvid
 		controlList.add(new SmallGuiButton(2, (width-xSize) / 2 + 138, (height - ySize) / 2 + 75, 30,10, "Paint"));
 		controlList.add(new SmallGuiButton(3, (width-xSize) / 2 + 47, (height - ySize) / 2 + 50, 37,10, "Import"));
 		controlList.add(new SmallGuiButton(4, (width-xSize) / 2 + 15, (height - ySize) / 2 + 50, 28,10, "Open"));
+		for(int i = 0; i < 6; i++) {
+			controlList.add(buttonarray[i] = new SmallGuiButton(5 + i, (width-xSize) / 2 + 20 + 18 * i, (height - ySize) / 2 + 37, 10,10, ">"));
+			buttonarray[i].drawButton = false;
+		}
 	}
 	
 	
 	@Override
 	protected void actionPerformed(GuiButton guibutton) {
+		if(5 < guibutton.id && guibutton.id < 11) {
+			handleStackMove(guibutton.id - 5);
+		}
 		switch(guibutton.id){
 		case 0:
 			_logic.setNextSatellite();
@@ -109,11 +119,17 @@ public class GuiCraftingPipe extends GuiContainer implements IGuiIDHandlerProvid
 		}
 
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		drawRect(0,1000,0,10000, 0xFFFF0000);
 		drawTexturedModalRect(155, 50, 10 * (xSize / 16) , 0, 10, 10);
 		MinecraftForgeClient.unbindTexture();
 		fontRenderer.drawString(""+_logic.satelliteId , 155 - fontRenderer.getStringWidth(""+_logic.satelliteId) , 52, 0x404040);
 	}
-
+	
+	@Override
+	public void drawScreen(int par1, int par2, float par3) {
+		super.drawScreen(par1, par2, par3);
+	}
+    
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float f, int x, int y) {
 		int i = mc.renderEngine.getTexture("/logisticspipes/gui/crafting.png");
@@ -125,6 +141,28 @@ public class GuiCraftingPipe extends GuiContainer implements IGuiIDHandlerProvid
 		drawTexturedModalRect(j, k, 0, 0, xSize, ySize);
 
 		drawRect(400, 400, 0, 0, 0x404040);
+
+		for(int count=36; count<42;count++) {
+			Slot slot = inventorySlots.getSlot(count);
+			if(slot != null && slot.getStack() != null && slot.getStack().getMaxStackSize() < 2) {
+				drawRect(guiLeft + 18 + (18 * (count-36)), guiTop + 18, guiLeft + 18 + (18 * (count-36)) + 16, guiTop + 18 + 16, 0xFFFF0000);
+				buttonarray[count - 36].drawButton = true;
+			} else {
+				buttonarray[count - 36].drawButton = false;
+			}
+		}
+	}
+
+	private void handleStackMove(int number) {
+		Slot slot = inventorySlots.getSlot(number + 36);
+		if(slot == null || slot.getStack() == null) return;
+		for(int i = 42;i < 45;i++) {
+			Slot slotb = inventorySlots.getSlot(i);
+			if(slotb != null && slotb.getStack() == null) {
+				slotb.putStack(slot.getStack());
+				slot.putStack(null);
+			}
+		}
 	}
 
 	@Override

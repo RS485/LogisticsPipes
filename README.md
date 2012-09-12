78396a0a7deab23c623f2cc56b11c386f048d6a0
Benedikt Ziemons <B.Ziemons@gmx.de>
2012-09-12 19:28:04 +0200

    Updated to new mcmod.info file format.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 #LogisticsPipes
 
-This is the **unofficial SMP version with additional features** for the minecraft modification *Logistics Pipes*.
+This is the **SMP version with additional features** for the minecraft modification *Logistics Pipes*.
 
 Logistics Pipes is copyrighted by [Krapht](https://github.com/Krapht "Krapht GitHub profile") and licensed under [MMPL-1.0](http://www.mod-buildcraft.com/MMPL-1.0.txt "Link to the license").
+
+It is not allowed to use this mod in any mod packs, if not explicit allowed.
 
 #Release
 
@@ -17,12 +19,6 @@ Logistics Pipes is copyrighted by [Krapht](https://github.com/Krapht "Krapht Git
 Krapht's Logistics Pipes is an extensive overhaul of the Buildcraft pipe system. It allows for better distribution of items via pipes, more organised stockkeeping and easier automated crafting.
 
 \- *Logistics Pipes Wiki* ( [http://logisticspipes.wikia.com](http://logisticspipes.wikia.com) )
-
-##Branches
-
-- `master` => Vanilla + BC 3.1.x
-- `BC2` => Vanilla + BC 2.2.x
-- `BC2-bukkit` => Bukkit + BC 2.2.x
 
 ##Added Features
 
--- common/logisticspipes/LogisticsPipes.java
+++ common/logisticspipes/LogisticsPipes.java
@@ -5,232 +5,8 @@
  * License 1.0, or MMPL. Please check the contents of the license located in
  * http://www.mod-buildcraft.com/MMPL-1.0.txt
  */
+
 /*
- 
-ChangeLog:
-	TODO: 
-		* REMOVE DEBUG CODE (always! ALL OF IT!)
-		* 
-		*
-		*
-  - 0.2.5
-  	Bug Fixes:
-  		* Fixed the delayed send by modules, hopefully this causes no other problems, let me know if it does!
-  		* Another attempt at fixing the concurrent iterator modification crash, hopefully the last time.
-  	New stuff:
-  		* Added Provider Modules - Like provider pipes, but modules! 
-  		* Added upgraded extractors - extracts once per second
-   
-  - 0.2.4
-  	Bug Fixes:
-  		* Repaired integration with AdditionalPipes (teleport pipes) 
-  		* Fixed the bottom 3 slots of MK5 chassis to not be configurable
-  		* Fixed chassis pipes not using the configured item ids
-  		* 
-  - 0.2.3
-	New stuff:
-		* Ported to MC 1.2.5
-	Bug fixes:
-		* Fixed a stackoverflow crash when itemstacks heading into a almost full default route couldn't fully fit
-
-	Known issues:
-		* There is probably still a ConcurrentModification crash in there somewhere, but I have not been able to reproduce it  
-  		
-  - 0.2.2
-  	New stuff:
-  		* Ported to MC 1.2.3
-  		* (3.x ONLY) Added liquid supplier pipes
-  		* Added chassis mk 5 - 8 module slots
-  		* Added Minor "Easter Egg"
-  		* 
-  	Bug fixes:
-  		* (2.x) Fixed 0 stack items being dropped after being buffered
-  		* Fixed the concurrent collection modification exception when splitting itemstacks that should be re-routed
-  		* Fixed Chassis pipes being disabled by the "disable pipe" gate action
-  		* Chassis now check ISidedInventory when checking if items will fit (when placed on top of a furnace it will hold a maximum of one stack)
-  	
-  - 0.2.1
-  
-  	New Stuff:
-  		* Added Terminus module. Identical in every aspect to the ItemSink, but has a lower priority, but still higher than a default route. I'm going to use this one on my recyclers/AMR
-  		* Renamed ChestItemSink to Polymorphic ItemSink
-  		* Added "sneaky" functionality to extractor modules. You can now specify from which direction they should extract, regardless of which direction it is actually connected.
-  		* Items are now slowed down if being dropped from a logisticspipe.
-  	Bug fixes:
-  		* Speed is now correct. No more lightningspeed for everything. Default routed items should be slowest, passive items faster and requested items fastest.
-  		* Items are sped up at the first pipe, as intended.
-
-  - 0.2.0 (Requires BC 2.1.12 or 3.1.3)
-  	
-  	New Stuff:
-  		* Yet again, massive massive internal rewrite of routing (different part though) so expect them bugs!
-  		* (3.x ONLY) Added alternative recipe for the basic logistics pipe. Can now be crafter using 2 golden chipsets instead of golden gears.
-  		* Logipipes will no longer instantly drop items it don't know where to send. It will now buffer them internally for 2 seconds and then retry.
-  		* 	If the re-send also fails, the item will be dropped. Also items arriving at their destination but have nowhere to go from there will be instantly dropped
-  		* 	This will hopefully fix the items being dropped on world loads.
-  		* Repurposed the requester pipe (also changed recipe to 2 gold gears and basic pipe). It will now open the remote orderer gui.
-  		* Requester pipe now also track item statistics over the last 20 minutes of play (resolution: once a minute). A graph can be viewed from the requester pipe GUi by pressing 'stats'. This info is not saved and is on a session basis only
-  		* (3.x ONLY) Added alternative recipe for requester pipe using golden chipsets instead of golden gears
-  		* Added chassis system
-  		* 	Unlike "normal" logipipes a chassi pipe can hold 1-4 modules (see below). Each chassis pipe needs to be aligned with an IInventory (shift-click with wrench to change) indicated by the orange stripe.
-  		* 	Chassis pipes will do a best-effort to avoid overflowing chests, and reroute as needed. It is however always a good idea to keep a default route in a non-overflowing chest 
-  		* Added chassis modules (working on names)
-  		* 	ItemSink module - Works like the basic logistics pipe (Internal prio: 1, Default route: 0)
-  		* 		- Use: Sort stuff
-  		* 	ChestItemSinkModule - Works like the basic logistics pipe, but without the need to configure it. Instead it will accept all items found in the aligned chest (Internal prio 1)
-  		* 		- Use: Sort stuff
-  		* 	Passive supplier - Works like the normal supplier, but will not activly request items, but it will keep a stock from passive item inflow (Internal prio 2)
-  		* 		- Use: Keep x amount of items saved, before they go to the normal ItemSinks
-  		* Additional modules will be added over time
-  		* Slightly increased the extraction speed of suppliers
-  		* Added different extraction modes to providers.
-  		* 	Leave first stack, leave last stack, leave first & last and leave 1 item per stack
-  		* Increased the extraction speed of crafting pipes to match the speed of suppliers
-  		* Added extractor module - This module will extract 1 item (not stacks) every 5 seconds from the adjacent inventory. This respects normal pipe-side operations (ISpecialInventory/ISidedInventory). Items will not be extracted if there is no route (Ie a specific or default) present in the network
-  		* Added quicksort module, this expensive, yet powerful, yet limited module will every 5 seconds pull all itemstacks from an inventory with size > 26. Only items that has a specific destination will be pulled, default routes does not count. It will not respect ISpecialInventory/ISidedinventory and will ruthlessly grab everything that has a route. 
-  		*	The limits are there on purpose as I don't want to see this module as a general pull-stuff-out automation module. It is intended to be used in combination with a chest to allow you a place to dump stuff for sorting. Any other shenanigans will make me nerf it/remove it.
-  		* Added 2 additional display modes to the order/requester GUI, you can now view Supply only or Craft only in addition to Both 
- 
-  	Bug fixes:
-  		* Fixed NPE when breaking a logisticspipe that has incoming items
-  		* (3.x ONLY) Adapt action code to new BC action code 
-
-
-  - 0.1.8
-  	New Stuff:
-  		* (3.x ONLY) Added custom actions to all logisticspipes that will disable them. They will still route and finish what they were doing, but will not provide/craft/supply/accept new items. Actions are accessed through gates. 
-  	Bug fixes:
-  		* Added ModLoader.getPriorities() that will hopefully resolve any remaining loading order problems and some blank texture issues.
-  		* Fixed several crashes related to removing logisticspipes while items are traveling
-  	
-  - 0.1.7
-  	New Stuff:
-  		* Massive refactoring of Routing code, expect bugs!
-  		* (3.x ONLY) Builder supplier pipe, a pipe that automatically supplies attached builder with items needed for the current section
-  		* Holding shift and scrolling the mouse wheel will now change pages in the orderer
-  		* Added config settings to invert mouse wheel scrolling direction for both pages and request count
-  		* (3.x ONLY) Supplier pipes should have a trigger for when they fail a request. This can be used with gates.
-  	Bug fixes:
-  		* Fixed crafting pipe GUI to not import when you click paint.
-  	Other:
-  		* Displaying the routes now requires holding shift (and without item equipped)
-  		* Shift-rightclicking on a dummy stack with an empty hand will now double the size (up to 127)
-  		* The remote orderer will now refresh item list on request, and will correctly not display items that have been reserved.
-  		* The remote orderer gui will now only clear the selected item if clicked within the darker square (roughly, not pixel accurate)
-  	
-  - 0.1.6
-  	Bug fixes:
-  		* Fixed incompatability with NEI on shift-clicking in configuration GUIs
-  		* Replaced client hang on crafting loops with a lag spike and error message
-  		* Changed max stack size in configuration guis to 127 as that is the maximum stack size that can be saved to world file.
-  		* 
-  	
-  - 0.1.5
- 
- 	New stuff:
- 		* The provider can now be configured to provide all items EXCEPT the ones in the filter
- 		* The crafting pipe can now import recipes. (ONLY works with automatic crafting tables)
- 		* The supplier can now be configured to do partial requests. If configured for it, it will first try requesting the full amount. Failing that it will request half the missing items, then half of that and so on down to 1 
- 	Bug fixes:
- 		* Fixed an issue with logisticsmanager not using extras/leftovers properly
- 		* Possible fix for nullpointerexception in sendFailed()
- 		* Suppliers will no longer try to supply inventories with size 0 and are explicitly forbidden to supply redstone engines.
- 	Other:
- 		* You can now use the mouse wheel to change the requested number of items in the GUI for RemoteOrderer (experimental)
- 		* The supplier will no longer supply while its GUI is open.
- 
- - 0.1.4
- 
- 	New stuff:
- 		* RemoteOrderer - Instead of a GUI for requester pipe you get a portable item. Point at any logisticspipe and you get your GUI. Lists all items and stock of all items in the current network that is connected to a provider. Items with a 0 are out of stock, but they might be able to be crafted. Items requested will be delivered to the logistics pipe you clicked on. Be aware that normal delivery rules apply. 1) Deliver to chest/inventory 2) Deliver to unrouted connection 3) Drop it 
- 		* Basic logisticspipe GUI and crafting GUI converted to use fake items. Upgrading to this version will convert your current items to fake ones and you will not get them back.
- 		* 
- 	Bug fixes:
- 		* Fixed nasty router bug (crash) due to lingering statics.
- 		* 
- 	Other:
- 		* All requests will now favor providers/crafters closer (in terms of number of pipes) to the point of the request. Previously this was a bit undefined(random).
- 		* Changed the Network Monitor sprite
- 		* Replaced diamond with glowstone dust in the recipes for provider/requester/crafter pipes (yes, anyone playing legit may TMI exchange the appropriate ammount of glowstone to diamonds as compensation)
- 		* Slightly increased the extraction speed of providers
-
- - 0.1.3
- 
- 	New stuff:
- 		* Provider pipes now have a filter gui. Putting items in it will ONLY provide those items. If empty it will provide all available items
- 		* Logisticspipes now detect and use additionalpipes' teleport pipes.
-
-- 0.1.2
-		
-	New stuff:
-		* Added Supplier pipe. Will try to keep an inventory supplied with the configured materials by requesting them from the network. Checks every 5 seconds 
-			(Do not place a provider pipe on the same chest and same network. It will not work!)
-		* Added network monitor. Using this on any logistic pipe will bring up some network statistics
-		* Testing different a type of interface for supplier pipe. You do not need to use real items to configure suppliers.
-		* Requester pipes and crafting pipes will now attempt to re-request any items that go missing
-		* Added a network monitor to track some stats on the pipes.,
-		* Some behind the scenes tech improvements to be closer to saving the routed items to the world file.
-
-	Bug fixes:
-		* Changed the route table calculations to lazy-load (on demand). Didn't see a point to completely 
-			recalculate the route table for every single router whenever the network changed. This should hopefully remove 
-			the massive lag spike some players are experiencing on maps with many pipes.
-		* Fixed provider pipes so they now work with double chests.	
-		* Fixed the speed that non-requested extras from crafting pipes are extracted at.
-		* Fixed routing for items tagged with a destination that cannot be reached. It will now attempt to route it somewhere else or drop it. 
-			This is to prevent items randomly flowing around the logistics network.
-	
-	Other:
-		* Items going to satellite pipes are now directly routed there
-		* Items crafted in furnace (or anything except crafting table) will now be picked up as soon as it is ready.
-		* Obsidian pipes will now (in addition to iron pipes) separate two networks 
-		* Routing is now much more strict. Items in logistics pipes will no longer sometimes default to buildcraft's standard way of directing items. 
-			If a pipe can not find a valid destination it will be dropped.
-
-  	Remaining Issues:
-		* Its possible to cause circular recursion by have a crafting pipe craft A using B, at the same time as having another crafting pipe crafting B using A. This will crash the game
-		* Multiple providers on same chest.
-		* Supplier and provider connected to same chest and network.
-		* 
-
-
- - 0.1.1
-	New stuff:
-		* Added support in the crafting pipe to most IInventories (furnaces, chests etc)
-		* Added an output message stating why a request failed. Easier to know what you ran out of, easier for me to debug.
-		* Added new pipe, satellite pipe, these can be connected to crafting pipes and, if enabled, will send incoming materials in the 3 rightmost slots to the satellite instead of through the crafting pipe.
-		* 
- 		
- 	Bug fixes:
- 		* Renamed zip file to solve issues on *nix (including Mac) when logisticspipes was loaded before buildcraft
- 		* Renamed zip file to solve texture issues with other mods, only appearing when the other mod loaded after buildcraft, but before logisticspipes
- 		* Did the right thing and added initiation of buildcraft, just in case this mod loads before buildcraft core.
- 		* Fixed recipe for crafting pipe now its "dPd", d = Diamond, P =Basic Logistics Pipe 
- 		* Routing no longer assumes that pipes next to each other actually are connected. (Cobblestone, smoothstone)
- 		* Routing also makes additional checks to ensure that we don't try and route over waterproof or conductive pipes.
- 		* Route painting has been moved to the same method that calculates the paths for routes making it more robust and accurate
- 		* Remove debug code accidently left in provider pipes (to set time to day)
- 		* Fixed bug where provider pipes extracts from connected pipes with inventory.
- 		* Logisticsmanager will now properly make use of extra resources produced within a request
- 		* Logisticsmanager will now make use of any extras from previous request that have not been crafted yet.
- 		* Redid some routing logic. All routers now share a single map of all routers instead of having its own. Let me know how this works out
- 		* Rightclicking on all all logisticspipes (except provider) will now allow placing of pipes
- 		
- 	Other:
- 		* Redesigned craftingpipe gui
- 		* Changed the default routed speed to 10x normal speed, up from 5x. Upgrading users will have to update their config file.
- 		
- - 0.1.0
- - Alternate texture for routed exits
- - Performance related fix that caused recalculation of shortest even when paths had not changed.
- - Fixed bug that was could cause items to slow down if it was inputed at speeds higher than the speed the router would send them.
- - "Fixed" routing bug - network wasn't converging when loading world
- - Iron pipes separate network segments
- - Crafting, need specify input->output for logistics in gui (dynamic recepies)
- - Logs to modloader.txt which texture ids it gets from buildcraft
-
- 
- 
 TODO later, maybe....
  - Status screen (in transit, waiting for craft, ready etc)
  - RoutedEntityItem, targetTile - specify which "chest" it should be delivered to
@@ -246,7 +22,7 @@ TODO later, maybe....
  	- Save logistics to file. Save coordinates so they can be resolved later. Also save items in transit and count them as not delivered
  - SMP:
 	- Peering, transport other peoples items. Need hook to set owner of PassiveEntity
- */
+*/
 
 package logisticspipes;
 
@@ -342,11 +118,9 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.common.registry.TickRegistry;
 
-@Mod(name="Logistics Pipes", version="0.5.@(Build_Number)", useMetadata = false, modid = "LP|MAIN")
+@Mod(modid = "LogisticsPipes|Main", name = "Logistics Pipes", version = "%VERSION%", useMetadata = true)
 @NetworkMod(channels = {NetworkConstants.LOGISTICS_PIPES_CHANNEL_NAME}, packetHandler = PacketHandler.class, clientSideRequired = true, serverSideRequired = true)
 public class LogisticsPipes {
-	
-	
 	public static LogisticsPipes instance;
 
 	//Log Requests
@@ -412,10 +186,6 @@ public class LogisticsPipes {
 		SimpleServiceLocator.setDirectConnectionManager(manager);
 		SimpleServiceLocator.setLogisticsManager(new LogisticsManagerV2());
 		SimpleServiceLocator.setInventoryUtilFactory(new InventoryUtilFactory());
-	}
-	
-	public String getPriorities() {
-		return "after:mod_BuildCraftCore;after:mod_BuildCraftTransport;after:mod_Forestry;after:mod_IC2";
 	}
 	
 	@Init
@@ -507,12 +277,7 @@ public class LogisticsPipes {
 				ModLoader.getLogger().fine("Additional pipes not detected: " + e1.getMessage());
 			}
 		}
-		
-		//BuildCraftCore.initialize();
-		//BuildCraftTransport.initialize();
-		//BuildCraftBuilders.initialize();
-		//BuildCraftSilicon.initialize();
-		
+				
 		LogisticsNetworkMonitior = new LogisticsItem(Configs.LOGISTICSNETWORKMONITOR_ID);
 		LogisticsNetworkMonitior.setIconIndex(Textures.LOGISTICSNETWORKMONITOR_ICONINDEX);
 		LogisticsNetworkMonitior.setItemName("networkMonitorItem");

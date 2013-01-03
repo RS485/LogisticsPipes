d720d93fab47c55dc04cce34d133dade1f3b3c48
davboecki <dboeken@yahoo.de>
2013-01-03 17:15:53 +0100

    Added Thaumcraft Aspect rendering.

---
+++ common/logisticspipes/proxy/thaumcraft/ThaumCraftProxy.java
@@ -0,0 +1,37 @@
+package logisticspipes.proxy.thaumcraft;
+
+import logisticspipes.proxy.MainProxy;
+import logisticspipes.proxy.interfaces.IThaumCraftProxy;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.item.ItemStack;
+
+import org.lwjgl.opengl.GL11;
+
+import thaumcraft.api.EnumTag;
+import thaumcraft.api.ObjectTags;
+import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.client.UtilsFX;
+import cpw.mods.fml.client.FMLClientHandler;
+
+public class ThaumCraftProxy implements IThaumCraftProxy {
+	
+	@Override
+	public void renderAspectsDown(ItemStack item, int x, int y, GuiScreen gui) {
+		if(!MainProxy.isClient()) return;
+		GL11.glPushMatrix();
+		Minecraft mc = FMLClientHandler.instance().getClient();
+		ObjectTags tags = ThaumcraftApi.getObjectTags(item);
+		tags = ThaumcraftApi.getBonusObjectTags(item, tags);
+		if (tags != null) {
+			int index = 0;
+			for (EnumTag tag : tags.getAspectsSorted()) {
+				int yPos = y + index * 18;
+				UtilsFX.drawTag(mc, x, yPos, tag, tags.getAmount(tag), gui, true, false);
+				index++;
+			}
+		}
+		GL11.glPopMatrix();
+	}
+
+}
---
+++ dummy/thaumcraft/api/ThaumcraftApi.java
@@ -0,0 +1,16 @@
+package thaumcraft.api;
+
+import net.minecraft.item.ItemStack;
+
+//Dummy Class
+public class ThaumcraftApi {
+	public static ObjectTags getObjectTags(ItemStack is) {
+		//Dummy Class
+		return null;
+	}
+
+	public static ObjectTags getBonusObjectTags(ItemStack item, ObjectTags tags) {
+		//Dummy Class
+		return null;
+	}
+}

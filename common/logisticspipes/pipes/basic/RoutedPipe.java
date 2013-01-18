6d16c29d5ee332a151d6edd840971287429ccc6e
ArtForz <artforz@localhost>
2013-01-18 19:35:17 +0100

    We don't care about neighbor changes if we're going to get replaced anyways

--- common/logisticspipes/pipes/basic/RoutedPipe.java
+++ common/logisticspipes/pipes/basic/RoutedPipe.java
@@ -32,7 +32,7 @@ public abstract class RoutedPipe extends CoreRoutedPipe {
 	@Override
 	public void onNeighborBlockChange(int blockId) {
 		super.onNeighborBlockChange(blockId);
-		if(MainProxy.isServer()) {
+		if(!stillNeedReplace && MainProxy.isServer()) {
 			onNeighborBlockChange_Logistics();
 		}
 	}

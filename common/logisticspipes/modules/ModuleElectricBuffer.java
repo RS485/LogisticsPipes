205ff99b4c75fae5275ed0ddf8a28c25b94e70b1
MrTJP <Tej@Tejs-MacBook-Pro.local>
2013-01-20 17:30:29 -0500

    Added missing files.

---
+++ common/logisticspipes/modules/ModuleElectricBuffer.java
@@ -0,0 +1,95 @@
+package logisticspipes.modules;
+
+import logisticspipes.interfaces.IChassiePowerProvider;
+import logisticspipes.interfaces.ILogisticsModule;
+import logisticspipes.interfaces.ISendRoutedItem;
+import logisticspipes.interfaces.IWorldProvider;
+import logisticspipes.logisticspipes.IInventoryProvider;
+import logisticspipes.pipefxhandlers.Particles;
+import logisticspipes.proxy.MainProxy;
+import logisticspipes.proxy.SimpleServiceLocator;
+import logisticspipes.utils.SinkReply;
+import logisticspipes.utils.SinkReply.FixedPriority;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class ModuleElectricBuffer implements ILogisticsModule {
+	private IInventoryProvider _invProvider;
+	private IChassiePowerProvider _power;
+	private ISendRoutedItem _itemSender;
+	private int xCoord;
+	private int yCoord;
+	private int zCoord;
+	private IWorldProvider _world;
+	
+	private int currentTickCount = 0;
+	private int ticksToAction = 100;
+
+	public ModuleElectricBuffer() {}
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbttagcompound) {}
+
+	@Override
+	public void writeToNBT(NBTTagCompound nbttagcompound) {}
+
+	@Override
+	public void registerHandler(IInventoryProvider invProvider, ISendRoutedItem itemSender, IWorldProvider world, IChassiePowerProvider powerProvider) {		
+		_invProvider = invProvider;
+		_power = powerProvider;
+		_world = world;
+		_itemSender = itemSender;
+	}
+
+	@Override
+	public void registerPosition(int xCoord, int yCoord, int zCoord, int slot) {
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+		this.zCoord = zCoord;
+	}
+
+	@Override
+	public SinkReply sinksItem(ItemStack stack) {
+		if (SimpleServiceLocator.IC2Proxy.isElectricItem(stack)) {
+			return positiveSinkReply();
+		}
+		return null;
+	}
+
+	private SinkReply positiveSinkReply() {
+		if (!_power.useEnergy(5)) return null;
+		SinkReply reply = new SinkReply();
+		reply.fixedPriority = FixedPriority.ElectricNetwork_Buffer;
+		reply.isPassive = true;
+		MainProxy.sendSpawnParticlePacket(Particles.BlueParticle, xCoord, yCoord, zCoord, _world.getWorld(), 2);
+		return reply;
+	}
+
+	@Override
+	public ILogisticsModule getSubModule(int slot) {
+		return null;
+	}
+
+	@Override
+	public void tick() {
+		if (++currentTickCount < ticksToAction) return;
+		currentTickCount = 0;
+		
+		IInventory inv = _invProvider.getInventory();
+		if (inv == null) return;
+		for (int i = 0; i < inv.getSizeInventory(); i++) {
+			ItemStack stack = inv.getStackInSlot(i);
+			if (stack == null) continue;
+			if (SimpleServiceLocator.IC2Proxy.isElectricItem(stack)) {
+				if (SimpleServiceLocator.logisticsManager.hasDestinationWithPriority(stack, _itemSender.getSourceUUID(), true, FixedPriority.ElectricNetwork_Manager)) {
+					MainProxy.sendSpawnParticlePacket(Particles.OrangeParticle, this.xCoord, this.yCoord, this.zCoord, _world.getWorld(), 2);
+					_itemSender.sendStack(inv.decrStackSize(i, 1));
+					return;
+				}
+			}
+			continue;
+		}
+	}
+
+}
---
+++ common/logisticspipes/proxy/ic2/IC2Proxy.java
@@ -0,0 +1,163 @@
+package logisticspipes.proxy.ic2;
+
+import ic2.api.IElectricItem;
+import ic2.api.Ic2Recipes;
+import ic2.api.Items;
+import logisticspipes.LogisticsPipes;
+import logisticspipes.items.ItemModule;
+import logisticspipes.proxy.interfaces.IIC2Proxy;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import buildcraft.BuildCraftCore;
+import buildcraft.BuildCraftSilicon;
+
+
+public class IC2Proxy implements IIC2Proxy {
+
+	/**
+	 * @return Boolean, true if itemstack is a ic2 electric item.
+	 * @param stack The stack to check.
+	 */
+	@Override
+	public boolean isElectricItem(ItemStack stack) {
+		return stack != null && stack.getItem() instanceof IElectricItem;
+	}
+
+	/**
+	 * @return Int value of current charge on electric item.
+	 * @param stack The stack to get charge for.
+	 */
+	@Override
+	public int getCharge(ItemStack stack) {
+		if (stack.getItem() instanceof IElectricItem && stack.hasTagCompound()) {
+			return stack.getTagCompound().getInteger("charge");
+		} else {
+			return 0;
+		}
+	}
+
+	/**
+	 * @return Int value of maximum charge on electric item.
+	 * @param stack The stack to get max charge for.
+	 */
+	@Override
+	public int getMaxCharge(ItemStack stack) {
+		if (!(stack.getItem() instanceof IElectricItem)) return 0;
+		return ((IElectricItem) stack.getItem()).getMaxCharge();
+	}
+
+	/**
+	 * @return Boolean, true if electric item is fully charged.
+	 * @param stack The stack to check if its fully charged.
+	 */
+	@Override
+	public boolean isFullyCharged(ItemStack stack) {
+		if (!isElectricItem(stack)) return false;
+		int charge = getCharge(stack);
+		int maxCharge = getMaxCharge(stack);
+		return charge == maxCharge;
+	}
+	
+	/**
+	 * @return Boolean, true if electric item is fully discharged.
+	 * @param stack The stack to check if its fully discharged.
+	 */
+	@Override
+	public boolean isFullyDischarged(ItemStack stack) {
+		if (!isElectricItem(stack)) return false;
+		int charge = getCharge(stack);
+		return charge == 0;
+	}
+	
+	/**
+	 * @return Boolean, true if electric item contains charge but is not full.
+	 * @param stack The stack to check if its partially chraged.
+	 */
+	public boolean isPartiallyCharged(ItemStack stack) {
+		return (!isFullyCharged(stack) && !isFullyDischarged(stack));
+	}
+	
+	/**
+	 * Adds crafting recipes to "IC2 Crafting"
+	 */
+	@Override
+	public void addCraftingRecipes() {
+		Ic2Recipes.addCraftingRecipe(new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.ELECTRICMANAGER), new Object[] { 
+			"CGD", 
+			"rBr", 
+			"DrC", 
+			Character.valueOf('C'), Items.getItem("electronicCircuit"),
+			Character.valueOf('D'), Items.getItem("reBattery"),
+			Character.valueOf('G'), BuildCraftCore.goldGearItem,
+			Character.valueOf('r'), Item.redstone,
+			Character.valueOf('B'), new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.BLANK)
+		});
+		
+		Ic2Recipes.addCraftingRecipe(new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.ELECTRICMANAGER), new Object[] { 
+			"CGD", 
+			"rBr", 
+			"DrC", 
+			Character.valueOf('C'), Items.getItem("electronicCircuit"),
+			Character.valueOf('D'), Items.getItem("chargedReBattery"),
+			Character.valueOf('G'), BuildCraftCore.goldGearItem,
+			Character.valueOf('r'), Item.redstone,
+			Character.valueOf('B'), new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.BLANK)
+		});
+		
+		Ic2Recipes.addCraftingRecipe(new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.ELECTRICMANAGER), new Object[] { 
+			"CGc", 
+			"rBr", 
+			"DrC", 
+			Character.valueOf('C'), Items.getItem("electronicCircuit"),
+			Character.valueOf('c'), Items.getItem("reBattery"),
+			Character.valueOf('D'), Items.getItem("chargedReBattery"),
+			Character.valueOf('G'), BuildCraftCore.goldGearItem,
+			Character.valueOf('r'), Item.redstone,
+			Character.valueOf('B'), new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.BLANK)
+		});
+		
+		Ic2Recipes.addCraftingRecipe(new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.ELECTRICMANAGER), new Object[] { 
+			"CGc", 
+			"rBr", 
+			"DrC", 
+			Character.valueOf('C'), Items.getItem("electronicCircuit"),
+			Character.valueOf('c'), Items.getItem("chargedReBattery"),
+			Character.valueOf('D'), Items.getItem("reBattery"),
+			Character.valueOf('G'), BuildCraftCore.goldGearItem,
+			Character.valueOf('r'), Item.redstone,
+			Character.valueOf('B'), new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.BLANK)
+		});
+
+		Ic2Recipes.addCraftingRecipe(new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.ELECTRICMANAGER), new Object[] { 
+			" G ", 
+			"rBr", 
+			"DrC", 
+			Character.valueOf('C'), Items.getItem("electronicCircuit"),
+			Character.valueOf('D'), Items.getItem("reBattery"),
+			Character.valueOf('G'), new ItemStack(BuildCraftSilicon.redstoneChipset, 1, 2),
+			Character.valueOf('r'), Item.redstone,
+			Character.valueOf('B'), new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.BLANK)
+		});
+		
+		Ic2Recipes.addCraftingRecipe(new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.ELECTRICMANAGER), new Object[] { 
+			" G ", 
+			"rBr", 
+			"DrC", 
+			Character.valueOf('C'), Items.getItem("electronicCircuit"),
+			Character.valueOf('D'), Items.getItem("chargedReBattery"),
+			Character.valueOf('G'), new ItemStack(BuildCraftSilicon.redstoneChipset, 1, 2),
+			Character.valueOf('r'), Item.redstone,
+			Character.valueOf('B'), new ItemStack(LogisticsPipes.ModuleItem, 1, ItemModule.BLANK)
+		});
+	}
+
+	/**
+	 * @return If IC2 is loaded, returns true.
+	 */
+	@Override
+	public boolean hasIC2() {
+		return true;
+	}
+
+
+}

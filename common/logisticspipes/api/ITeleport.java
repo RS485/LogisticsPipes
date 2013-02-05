99562c22859020b1201fe5209ec911e60697a9d3
Andrew Hill <andy.t.roo@gmail.com>
2013-02-05 18:38:29 +1000

    actually compile before commiting

--- common/logisticspipes/api/ITeleport.java
+++ common/logisticspipes/api/ITeleport.java
@@ -1,30 +1,13 @@
 package logisticspipes.api;
  
+import java.util.Collection;
 import java.util.List;
  
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
  
 import buildcraft.api.transport.IPipedItem;
-import buildcraft.api.transport.IPipeEntry;
 import buildcraft.api.transport.IPipeConnection;
- 
-/** implemented by some Logistics pipes, and anyone else who cares to notice ITeleporters changing next to them.
- * extends IPipeEntry, to aid in efficient finding of destinations.
-  */
-public interface ICareAboutTeleport extends IPipeEntry  {
-        /* notify this pipe that teleport destinations have changed.
-         * @param changed the adjacent ITeleport that now links somewhere else.
-         */
-        void onAdjacentITeleportRouteChange(ITeleport changed);
- 
-        /* used if the adjacent ITeleport becomes temporarily disabled
-         * (full, unpowered, turned off with redstone, etc..)
-         * does not (immediately) trigger a routing update. (prolonged deactivation may)
-         **** Tentative function, may not exist in final API
-         */  
-        void onAdjacentITeleportTemporaryDisable(ITeleport changed,boolean isEnabled);
-}
  
 /**
  * Public interface implemented by TilesEntities which provide Teleport capabilities, and wish

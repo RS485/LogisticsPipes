cde31167b748d9af30502b4cbe6c9dc756a443e6
ArtForz <artforz@localhost>
2013-02-13 01:54:46 +0100

    Change ModBasedItemsink GUI, limit to 9 mods

--- common/logisticspipes/gui/modules/GuiModBasedItemSink.java
+++ common/logisticspipes/gui/modules/GuiModBasedItemSink.java
@@ -16,7 +16,6 @@ public class GuiModBasedItemSink extends GuiWithPreviousGuiContainer {
 	
 	private final ModuleModBasedItemSink _itemSink;
 	private final SimpleInventory tmpInv;
-	private int page = 0;
 	
 	public GuiModBasedItemSink(IInventory playerInventory, Pipe pipe, ModuleModBasedItemSink itemSink, GuiScreen previousGui, int slot) {
 		super(null, pipe, previousGui);
@@ -26,13 +25,13 @@ public class GuiModBasedItemSink extends GuiWithPreviousGuiContainer {
 		tmpInv = new SimpleInventory(1, "Analyse Slot", 1);
 		
 		DummyContainer dummy = new DummyContainer(playerInventory, tmpInv);
-		dummy.addDummySlot(0, 8, 8);
+		dummy.addDummySlot(0, 7, 8);
 		
-		dummy.addNormalSlotsForPlayerInventory(7, 100);
+		dummy.addNormalSlotsForPlayerInventory(7, 126);
 	    
 	    this.inventorySlots = dummy;
 		xSize = 175;
-		ySize = 182;
+		ySize = 208;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -40,9 +39,10 @@ public class GuiModBasedItemSink extends GuiWithPreviousGuiContainer {
 	public void initGui() {
 		super.initGui();
 		this.controlList.clear();
-		this.controlList.add(new GuiButton(0, guiLeft + 120, guiTop + 5, 50, 20, "Add"));
-		this.controlList.add(new SmallGuiButton(1, guiLeft + 135, guiTop + 30, 10, 10, "<"));
-		this.controlList.add(new SmallGuiButton(2, guiLeft + 155, guiTop + 30, 10, 10, ">"));
+		this.controlList.add(new SmallGuiButton(0, guiLeft + 38, guiTop + 18, 50, 10, "Add"));
+		this.controlList.add(new SmallGuiButton(1, guiLeft + 107, guiTop + 18, 50, 10, "Remove"));
+		((GuiButton)controlList.get(0)).enabled = false;
+		((GuiButton)controlList.get(1)).enabled = false;
 	}
 
 	@Override
@@ -53,19 +53,14 @@ public class GuiModBasedItemSink extends GuiWithPreviousGuiContainer {
 	@Override
 	protected void actionPerformed(GuiButton par1GuiButton) {
 		if(par1GuiButton.id == 0) {
+			if(!_itemSink.modList.contains(ItemIdentifier.get(tmpInv.getStackInSlot(0)).getModName())) {
+				_itemSink.modList.add(ItemIdentifier.get(tmpInv.getStackInSlot(0)).getModName());
+				_itemSink.ModListChanged();
+			}
+		} else if(par1GuiButton.id == 1) {
 			if(_itemSink.modList.contains(ItemIdentifier.get(tmpInv.getStackInSlot(0)).getModName())) {
 				_itemSink.modList.remove(ItemIdentifier.get(tmpInv.getStackInSlot(0)).getModName());
-			} else {
-				_itemSink.modList.add(ItemIdentifier.get(tmpInv.getStackInSlot(0)).getModName());
-			}
-			_itemSink.ModListChanged();
-			page = 0;
-		} else if(par1GuiButton.id == 1) {
-			page = Math.max(0, page - 1);
-		} else if(par1GuiButton.id == 2) {
-			page++;
-			if(page * 6 >= _itemSink.modList.size()) {
-				page--;
+				_itemSink.ModListChanged();
 			}
 		} else {
 			super.actionPerformed(par1GuiButton);			
@@ -75,22 +70,28 @@ public class GuiModBasedItemSink extends GuiWithPreviousGuiContainer {
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3) {
 		BasicGuiHelper.drawGuiBackGround(mc, guiLeft, guiTop, right, bottom, zLevel, true);
-		BasicGuiHelper.drawPlayerInventoryBackground(mc, guiLeft + 7, guiTop + 100);
-		BasicGuiHelper.drawSlotBackground(mc, guiLeft + 7, guiTop + 7);
+		BasicGuiHelper.drawPlayerInventoryBackground(mc, guiLeft + 7, guiTop + 126);
+		BasicGuiHelper.drawSlotBackground(mc, guiLeft + 6, guiTop + 7);
+		BasicGuiHelper.drawRect(guiLeft + 26, guiTop + 5, guiLeft + 169, guiTop + 17, 0xff808080);
 		if(tmpInv.getStackInSlot(0) != null) {
-			mc.fontRenderer.drawString(ItemIdentifier.get(tmpInv.getStackInSlot(0)).getModName(), guiLeft + 28, guiTop + 13, 0x404040);
+			mc.fontRenderer.drawString(ItemIdentifier.get(tmpInv.getStackInSlot(0)).getModName(), guiLeft + 28, guiTop + 7, 0x404040);
 			if(_itemSink.modList.contains(ItemIdentifier.get(tmpInv.getStackInSlot(0)).getModName())) {
-				((GuiButton)controlList.get(0)).displayString = "Remove";
+				((GuiButton)controlList.get(0)).enabled = false;
+				((GuiButton)controlList.get(1)).enabled = true;
+			} else if (_itemSink.modList.size() < 9) {
+				((GuiButton)controlList.get(0)).enabled = true;
+				((GuiButton)controlList.get(1)).enabled = false;
 			} else {
-				((GuiButton)controlList.get(0)).displayString = "Add";
+				((GuiButton)controlList.get(0)).enabled = false;
+				((GuiButton)controlList.get(1)).enabled = false;
 			}
-			((GuiButton)controlList.get(0)).drawButton = true;
 		} else {
-			((GuiButton)controlList.get(0)).drawButton = false;
+			((GuiButton)controlList.get(0)).enabled = false;
+			((GuiButton)controlList.get(1)).enabled = false;
 		}
-		BasicGuiHelper.drawRect(guiLeft + 5, guiTop + 28, guiLeft + 169, guiTop + 97, 0xff808080);
-		for(int i=(page * 6); i < _itemSink.modList.size() && i < 6 + (page * 6);i++) {
-			mc.fontRenderer.drawString(_itemSink.modList.get(i), guiLeft + 8, guiTop + 32 + (11 * (i - (page * 6))), 0x404040);
+		BasicGuiHelper.drawRect(guiLeft + 5, guiTop + 30, guiLeft + 169, guiTop + 122, 0xff808080);
+		for(int i=0; i < _itemSink.modList.size() && i < 9;i++) {
+			mc.fontRenderer.drawString(_itemSink.modList.get(i), guiLeft + 7, guiTop + 32 + (10 * i), 0x404040);
 		}
 	}
 }

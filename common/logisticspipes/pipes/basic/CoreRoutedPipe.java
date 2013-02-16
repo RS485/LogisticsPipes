f00f9af3d902595886cfa62fb49b779d07c82fde
Andrew Hill <andy.t.roo@gmail.com>
2013-02-16 21:57:49 +1000

    missing import after merge

--- common/logisticspipes/pipes/basic/CoreRoutedPipe.java
+++ common/logisticspipes/pipes/basic/CoreRoutedPipe.java
@@ -64,6 +64,7 @@ import logisticspipes.utils.Pair3;
 import logisticspipes.utils.WorldUtil;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;

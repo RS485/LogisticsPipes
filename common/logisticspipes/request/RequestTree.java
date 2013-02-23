2944e06a00a78d2d349b71a7d39fcedae7378eb0
ArtForz <artforz@localhost>
2013-02-23 19:44:54 +0100

    Convert RequestTree static recursers to TreeNode methods. Change getExtrasFor to internally use a hashmap. FullFill[sic] now recurses first.

--- common/logisticspipes/request/RequestTree.java
+++ common/logisticspipes/request/RequestTree.java
@@ -1,12 +1,12 @@
 package logisticspipes.request;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
-import logisticspipes.interfaces.routing.ICraftItems;
 import logisticspipes.interfaces.routing.IProvideItems;
 import logisticspipes.interfaces.routing.IRequestItems;
 import logisticspipes.routing.LogisticsExtraPromise;
-import logisticspipes.routing.LogisticsPromise;
 import logisticspipes.utils.ItemIdentifier;
 import logisticspipes.utils.ItemIdentifierStack;
 import logisticspipes.utils.ItemMessage;
@@ -18,130 +18,38 @@ public class RequestTree extends RequestTreeNode {
 	}
 
 	public int getAllPromissesFor(IProvideItems provider, ItemIdentifier item) {
-		return checkSubPromisses(provider, this, item);
-	}
-	
-	private static int checkSubPromisses(IProvideItems provider, RequestTreeNode node, ItemIdentifier item) {
-		int total = 0;
-		for(LogisticsPromise promise: node.promises) {
-			if(promise.sender == provider && promise.item == item) {
-				total += promise.numberOfItems;
-			}
-		}
-		for(RequestTreeNode subNode:node.subRequests) {
-			total += checkSubPromisses(provider, subNode, item);
-		}
-		return total;
+		return checkSubPromisses(provider, item);
 	}
 	
 	public LinkedList<LogisticsExtraPromise> getExtrasFor(ItemIdentifier item) {
+		HashMap<IProvideItems,List<LogisticsExtraPromise>> extraMap = new HashMap<IProvideItems,List<LogisticsExtraPromise>>();
+		checkForExtras(item,extraMap);
+		removeUsedExtras(item,extraMap);
 		LinkedList<LogisticsExtraPromise> extras = new LinkedList<LogisticsExtraPromise>();
-		checkForExtras(item,this,extras);
-		removeUsedExtras(item,this,extras);
+		for(List<LogisticsExtraPromise> sublist:extraMap.values()) {
+			extras.addAll(sublist);
+		}
 		return extras;
 	}
 
-	private static void checkForExtras(ItemIdentifier item, RequestTreeNode node, LinkedList<LogisticsExtraPromise> extras) {
-		for(LogisticsExtraPromise extra:node.extrapromises) {
-			if(extra.item == item) {
-				extras.add(extra.copy());
-			}
-		}
-		for(RequestTreeNode subNode:node.subRequests) {
-			checkForExtras(item,subNode,extras);
-		}
-	}
-
-	private static void removeUsedExtras(ItemIdentifier item, RequestTreeNode node, LinkedList<LogisticsExtraPromise> extras) {
-		for(LogisticsPromise promise:node.promises) {
-			if(promise.item != item) continue;
-			if(!(promise instanceof LogisticsExtraPromise)) continue;
-			LogisticsExtraPromise epromise = (LogisticsExtraPromise)promise;
-			if(epromise.provided) continue;
-			int usedcount = epromise.numberOfItems;
-			for(LogisticsExtraPromise extra : extras) {
-				if(extra.sender == epromise.sender) {
-					if(extra.numberOfItems >= usedcount) {
-						extra.numberOfItems -= usedcount;
-						usedcount = 0;
-						break;
-					} else {
-						usedcount -= extra.numberOfItems;
-						extra.numberOfItems = 0;
-					}
-				}
-			}
-		}
-		for(RequestTreeNode subNode:node.subRequests) {
-			removeUsedExtras(item,subNode,extras);
-		}
-	}
-
 	public void fullFillAll() {
-		fullFill(this);
+		fullFill();
 	}
 	
-	private static void fullFill(RequestTreeNode node) {
-		for(LogisticsPromise promise:node.promises) {
-			promise.sender.fullFill(promise, node.target);
-		}
-		for(LogisticsPromise promise:node.extrapromises) {
-			if(promise.sender instanceof ICraftItems) {
-				((ICraftItems)promise.sender).registerExtras(promise.numberOfItems);
-			}
-		}
-		for(RequestTreeNode subNode:node.subRequests) {
-			fullFill(subNode);
-		}
-	}
-
 	public void sendMissingMessage(RequestLog log) {
 		LinkedList<ItemMessage> missing = new LinkedList<ItemMessage>();
-		sendMissingMessage(missing, this);
+		sendMissingMessage(missing);
 		ItemMessage.compress(missing);
 		log.handleMissingItems(missing);
 	}
 
-	private void sendMissingMessage(LinkedList<ItemMessage> missing, RequestTreeNode node) {
-		if(node.getMissingItemCount() != 0) {
-			ItemIdentifierStack stack = node.getStack().clone();
-			stack.stackSize = node.getMissingItemCount();
-			missing.add(new ItemMessage(stack));
-		}
-		for(RequestTreeNode subNode:node.subRequests) {
-			sendMissingMessage(missing, subNode);
-		}
-	}
-	
 	public void sendUsedMessage(RequestLog log) {
 		LinkedList<ItemMessage> used = new LinkedList<ItemMessage>();
 		LinkedList<ItemMessage> missing = new LinkedList<ItemMessage>();
-		sendUsedMessage(used, missing, this);
+		sendUsedMessage(used, missing);
 		ItemMessage.compress(used);
 		ItemMessage.compress(missing);
 		log.handleSucessfullRequestOfList(used);
 		log.handleMissingItems(missing);
-	}
-
-	private void sendUsedMessage(LinkedList<ItemMessage> used, LinkedList<ItemMessage> missing, RequestTreeNode node) {
-		int usedcount = 0;
-		for(LogisticsPromise promise:node.promises) {
-			if(promise.sender instanceof IProvideItems && !(promise.sender instanceof ICraftItems)) {
-				usedcount += promise.numberOfItems;
-			}
-		}
-		if(usedcount != 0) {
-			ItemIdentifierStack stack = node.getStack().clone();
-			stack.stackSize = usedcount;
-			used.add(new ItemMessage(stack));
-		}
-		if(node.getMissingItemCount() != 0) {
-			ItemIdentifierStack stack = node.getStack().clone();
-			stack.stackSize = node.getMissingItemCount();
-			missing.add(new ItemMessage(stack));
-		}
-		for(RequestTreeNode subNode:node.subRequests) {
-			sendUsedMessage(used, missing, subNode);
-		}
 	}
 }
--- common/logisticspipes/request/RequestTreeNode.java
+++ common/logisticspipes/request/RequestTreeNode.java
@@ -1,16 +1,22 @@
 package logisticspipes.request;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import logisticspipes.interfaces.routing.ICraftItems;
+import logisticspipes.interfaces.routing.IProvideItems;
 import logisticspipes.interfaces.routing.IRelayItem;
 import logisticspipes.interfaces.routing.IRequestItems;
 import logisticspipes.routing.LogisticsExtraPromise;
 import logisticspipes.routing.LogisticsPromise;
+import logisticspipes.utils.ItemIdentifier;
 import logisticspipes.utils.ItemIdentifierStack;
+import logisticspipes.utils.ItemMessage;
 
 public class RequestTreeNode {
 
@@ -96,5 +102,109 @@ public class RequestTreeNode {
 
 	public boolean remove(RequestTreeNode subNode) {
 		return subRequests.remove(subNode);
+	}
+
+
+	/* RequestTree helpers */
+	protected int checkSubPromisses(IProvideItems provider, ItemIdentifier item) {
+		int total = 0;
+		for(LogisticsPromise promise: promises) {
+			if(promise.sender == provider && promise.item == item) {
+				total += promise.numberOfItems;
+			}
+		}
+		for(RequestTreeNode subNode:subRequests) {
+			total += subNode.checkSubPromisses(provider, item);
+		}
+		return total;
+	}
+
+	protected void checkForExtras(ItemIdentifier item, HashMap<IProvideItems,List<LogisticsExtraPromise>> extraMap) {
+		for(LogisticsExtraPromise extra:extrapromises) {
+			if(extra.item == item) {
+				List<LogisticsExtraPromise> extras = extraMap.get(extra.sender);
+				if(extras == null) {
+					extras = new LinkedList<LogisticsExtraPromise>();
+					extraMap.put(extra.sender, extras);
+				}
+				extras.add(extra.copy());
+			}
+		}
+		for(RequestTreeNode subNode:subRequests) {
+			subNode.checkForExtras(item, extraMap);
+		}
+	}
+
+	protected void removeUsedExtras(ItemIdentifier item, HashMap<IProvideItems,List<LogisticsExtraPromise>> extraMap) {
+		for(LogisticsPromise promise:promises) {
+			if(promise.item != item) continue;
+			if(!(promise instanceof LogisticsExtraPromise)) continue;
+			LogisticsExtraPromise epromise = (LogisticsExtraPromise)promise;
+			if(epromise.provided) continue;
+			int usedcount = epromise.numberOfItems;
+			List<LogisticsExtraPromise> extras = extraMap.get(epromise.sender);
+			if(extras == null) continue;
+			for(Iterator<LogisticsExtraPromise> it = extras.iterator(); it.hasNext();) {
+				LogisticsExtraPromise extra = it.next();
+				if(extra.numberOfItems >= usedcount) {
+					extra.numberOfItems -= usedcount;
+					usedcount = 0;
+					break;
+				} else {
+					usedcount -= extra.numberOfItems;
+					it.remove();
+				}
+			}
+		}
+		for(RequestTreeNode subNode:subRequests) {
+			subNode.removeUsedExtras(item,extraMap);
+		}
+	}
+
+	protected void fullFill() {
+		for(RequestTreeNode subNode:subRequests) {
+			subNode.fullFill();
+		}
+		for(LogisticsPromise promise:promises) {
+			promise.sender.fullFill(promise, target);
+		}
+		for(LogisticsPromise promise:extrapromises) {
+			if(promise.sender instanceof ICraftItems) {
+				((ICraftItems)promise.sender).registerExtras(promise.numberOfItems);
+			}
+		}
+	}
+
+	protected void sendMissingMessage(LinkedList<ItemMessage> missing) {
+		if(getMissingItemCount() != 0) {
+			ItemIdentifierStack stack = getStack().clone();
+			stack.stackSize = getMissingItemCount();
+			missing.add(new ItemMessage(stack));
+		}
+		for(RequestTreeNode subNode:subRequests) {
+			subNode.sendMissingMessage(missing);
+		}
+	}
+
+	protected void sendUsedMessage(LinkedList<ItemMessage> used, LinkedList<ItemMessage> missing) {
+		int usedcount = 0;
+		for(LogisticsPromise promise:promises) {
+			if(promise.sender instanceof IProvideItems && !(promise.sender instanceof ICraftItems)) {
+				usedcount += promise.numberOfItems;
+			}
+		}
+		if(usedcount != 0) {
+			ItemIdentifierStack stack = getStack().clone();
+			stack.stackSize = usedcount;
+			used.add(new ItemMessage(stack));
+		}
+		if(getMissingItemCount() != 0) {
+			ItemIdentifierStack stack = getStack().clone();
+			stack.stackSize = getMissingItemCount();
+			missing.add(new ItemMessage(stack));
+		}
+		for(RequestTreeNode subNode:subRequests) {
+			subNode.sendUsedMessage(used, missing);
+		}
 	}
 }

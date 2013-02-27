b50e95b287e92869be81d4295769f8ad07b84c54
MrTJP <Tej@Tejs-MacBook-Pro.local>
2013-02-26 22:14:03 -0500

    Gates
    - New conditional if item has logistic destination excluding default
    - Only shows on gates with parameters (Diamond)

---
+++ common/logisticspipes/gates/TriggerHasDestination.java
@@ -0,0 +1,57 @@
+package logisticspipes.gates;
+
+import java.util.ArrayList;
+
+import logisticspipes.pipes.basic.CoreRoutedPipe;
+import logisticspipes.proxy.SimpleServiceLocator;
+import logisticspipes.textures.Textures;
+import logisticspipes.utils.ItemIdentifier;
+import net.minecraft.item.ItemStack;
+import buildcraft.api.gates.ITriggerParameter;
+import buildcraft.api.gates.Trigger;
+import buildcraft.transport.Gate;
+import buildcraft.transport.ITriggerPipe;
+import buildcraft.transport.Pipe;
+
+public class TriggerHasDestination extends Trigger implements ITriggerPipe {
+
+	public TriggerHasDestination(int id) {
+		super(id);
+	}
+
+	@Override
+	public int getIndexInTexture() {
+		return 2 * 16 + 0;
+	}
+	
+	@Override
+	public boolean hasParameter() {
+		return true;
+	}
+	
+	@Override
+	public String getDescription() {
+		return "Item has destination";
+	}
+
+	@Override
+	public boolean isTriggerActive(Pipe pipe, ITriggerParameter parameter) {
+		if (pipe instanceof CoreRoutedPipe) {
+			if (parameter != null && parameter.getItem() != null) {
+				ItemStack item = parameter.getItem();
+				if (SimpleServiceLocator.logisticsManager.hasDestination(ItemIdentifier.get(item), false, ((CoreRoutedPipe) pipe).getRouter().getSimpleID(), new ArrayList<Integer>()) != null) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String getTextureFile() {
+		return Textures.LOGISTICSACTIONTRIGGERS_TEXTURE_FILE;
+	}
+	
+	
+
+}

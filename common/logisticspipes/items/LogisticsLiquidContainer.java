47caa3c284c665be809e3e0bd6a1052d12514f7e
MrTJP <tej1996@me.com>
2013-04-21 00:48:25 -0400

    Liquid Containers
    - Now dynamically change their icons based on the liquid they contain.

--- common/logisticspipes/items/LogisticsLiquidContainer.java
+++ common/logisticspipes/items/LogisticsLiquidContainer.java
@@ -6,10 +6,13 @@ import logisticspipes.interfaces.IItemAdvancedExistance;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class LogisticsLiquidContainer extends LogisticsItem implements IItemAdvancedExistance {
+	static int capacity = 8000;
+	
 	public LogisticsLiquidContainer(int i) {
 		super(i);
 	}
@@ -26,18 +29,51 @@ public class LogisticsLiquidContainer extends LogisticsItem implements IItemAdva
 
 	@Override
     @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister par1IconRegister)
-    {
-		//this.iconIndex = Textures.LOGISTICSITEM_LIQUIDCONTAINER_ICONINDEX;
+	public void registerIcons(IconRegister iconReg) {
+		for (LiquidIconProvider liquids : LiquidIconProvider.values()) {
+			if ((liquids.liquidIcon == null)) {
+				liquids.liquidIcon = iconReg.registerIcon("logisticspipes:" + "liquids/" + liquids.liquidID);
+			}
+		}
+		this.itemIcon = LiquidIconProvider.EMPTY.liquidIcon;
 	}
 	
+    /**
+     * Gets an icon index based on an item's damage value and the given render pass
+     */
 	@Override
-	@SideOnly(Side.CLIENT)
-	@SuppressWarnings("rawtypes")
-	public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List par3List) {}
-	
+    public Icon getIconFromDamageForRenderPass(int damage, int pass) {
+    	Icon icon = this.itemIcon;
+    	if (damage != 0 && pass == 0) {
+    		icon = LiquidIconProvider.values()[java.lang.Math.max(0, java.lang.Math.min(damage, LiquidIconProvider.values().length - 1))].liquidIcon;
+    	}
+    	return icon;
+    }
+    	
 	@Override
 	public int getItemStackLimit() {
 		return 1;
 	}
+	
+    /**
+     * returns a list of items with the same ID, but different meta (eg: dye returns 16 items)
+     */
+	public void getSubItems(int par1, CreativeTabs ct, List list) {
+		for (LiquidIconProvider liquids : LiquidIconProvider.values()) {
+			if (liquids.available) {
+				list.add(new ItemStack(this, 1, liquids.ordinal()));
+			}
+		}
+	}
+	
+	@Override
+    @SideOnly(Side.CLIENT)
+    public boolean requiresMultipleRenderPasses() {
+        return true;
+    }
+	
+	@Override
+    public int getRenderPasses(int metadata) {
+        return 2;
+    }
 }

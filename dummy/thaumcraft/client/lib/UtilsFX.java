653c26762374c7132d18985b0551921ef27104f0
davboecki <dboeken@yahoo.de>
2013-04-25 13:24:37 +0200

    Add UtilsFX to dummy files.

---
+++ dummy/thaumcraft/client/lib/UtilsFX.java
@@ -0,0 +1,10 @@
+package thaumcraft.client.lib;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import thaumcraft.api.EnumTag;
+
+public class UtilsFX {
+	public static void drawTag(Minecraft mc, int x, int y, EnumTag tag, int amount, GuiScreen gui, boolean back, boolean bw) {}
+	public static void drawTag(Minecraft mc, int x, int y, EnumTag tag, int amount, int bonus, GuiScreen gui, boolean back, boolean bw, float opacity) {}
+}

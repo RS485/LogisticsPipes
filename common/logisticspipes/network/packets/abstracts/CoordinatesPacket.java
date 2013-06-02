68542892005704a5da4740a054291d26e81b0f24
Guilherme Espada <porcariadagata@gmail.com>
2013-06-02 15:52:42 +0100

    Add abstract CoordinatesPacket.

---
+++ common/logisticspipes/network/packets/abstracts/CoordinatesPacket.java
@@ -0,0 +1,38 @@
+package logisticspipes.network.packets.abstracts;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public abstract class CoordinatesPacket extends ModernPacket {
+
+	public CoordinatesPacket(int id, int x, int y, int z) {
+		super(id);
+
+		posX = x;
+		posY = y;
+		posZ = z;
+	}
+
+	public int posX;
+	public int posY;
+	public int posZ;
+
+	@Override
+	public void writeData(DataOutputStream data) throws IOException {
+
+		data.writeInt(posX);
+		data.writeInt(posY);
+		data.writeInt(posZ);
+	}
+
+	@Override
+	public void readData(DataInputStream data) throws IOException {
+
+		posX = data.readInt();
+		posY = data.readInt();
+		posZ = data.readInt();
+
+	}
+
+}

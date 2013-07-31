5d13fef746c3ea73b2ed6e777312723eb5f51b25
ArtForz <artforz@artforz.ath.cx>
2013-07-31 03:46:33 +0200

    Remove unused CraftingLoop packet

--- common/logisticspipes/logisticspipes/MessageManager.java
+++ common/logisticspipes/logisticspipes/MessageManager.java
@@ -4,25 +4,14 @@ import java.util.LinkedList;
 
 import logisticspipes.network.PacketHandler;
 import logisticspipes.network.packets.orderer.ComponentList;
-import logisticspipes.network.packets.orderer.CraftingLoop;
 import logisticspipes.network.packets.orderer.MissingItems;
 import logisticspipes.proxy.MainProxy;
-import logisticspipes.utils.ItemIdentifier;
 import logisticspipes.utils.ItemMessage;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.network.Player;
 
 public class MessageManager {
 	
-	public static void overflow(EntityPlayer player, ItemIdentifier item) {
-		LinkedList<ItemMessage> error = new LinkedList<ItemMessage>();
-		error.add(new ItemMessage(item.itemID, item.itemDamage, 1, item.tag));
-//TODO Must be handled manualy
-		MainProxy.sendPacketToPlayer(PacketHandler.getPacket(CraftingLoop.class).setStack(new ItemStack(error.get(0).id, error.get(0).data, error.get(0).amount)), (Player)player);
-		//MainProxy.sendPacketToPlayer(new PacketCraftingLoop(error), (Player)player);
-	}
-
 	public static void errors(EntityPlayer player, LinkedList<ItemMessage> errors) {
 //TODO 	MainProxy.sendPacketToPlayer(new PacketItems(NetworkConstants.MISSING_ITEMS, errors,true).getPacket(), (Player)player);
 		MainProxy.sendPacketToPlayer(PacketHandler.getPacket(MissingItems.class).setItems(errors).setFlag(true), (Player)player);

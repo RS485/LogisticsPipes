227d7235e4f934714654152955cd930f6231aca3
ArtForz <artforz@artforz.ath.cx>
2013-08-01 16:27:25 +0200

    Don't sparkle if something is using power via useEnergy(amount, providersToIgnore)

--- common/logisticspipes/pipes/basic/CoreRoutedPipe.java
+++ common/logisticspipes/pipes/basic/CoreRoutedPipe.java
@@ -913,7 +913,7 @@ public abstract class CoreRoutedPipe extends Pipe implements IRequestItems, IAdj
 	
 	@Override
 	public boolean useEnergy(int amount){
-		return useEnergy(amount, null);
+		return useEnergy(amount, null, true);
 	}
 	@Override
 	public boolean canUseEnergy(int amount){
@@ -939,6 +939,10 @@ public abstract class CoreRoutedPipe extends Pipe implements IRequestItems, IAdj
 	
 	@Override
 	public boolean useEnergy(int amount, List<Object> providersToIgnore) {
+		return useEnergy(amount, providersToIgnore, false);
+	}
+
+	private boolean useEnergy(int amount, List<Object> providersToIgnore, boolean sparkles) {
 		if(MainProxy.isClient(worldObj)) return false;
 		if(Configs.LOGISTICS_POWER_USAGE_DISABLED) return true;
 		if(amount == 0) return true;
@@ -952,11 +956,13 @@ public abstract class CoreRoutedPipe extends Pipe implements IRequestItems, IAdj
 		for(ILogisticsPowerProvider provider: list) {
 			if(provider.canUseEnergy(amount, providersToIgnore)) {
 				provider.useEnergy(amount, providersToIgnore);
-				int particlecount = amount;
-				if (particlecount > 10) {
-					particlecount = 10;
+				if(sparkles) {
+					int particlecount = amount;
+					if (particlecount > 10) {
+						particlecount = 10;
+					}
+					MainProxy.sendSpawnParticlePacket(Particles.GoldParticle, this.getX(), this.getY(), this.getZ(), this.worldObj, particlecount);
 				}
-				MainProxy.sendSpawnParticlePacket(Particles.GoldParticle, this.getX(), this.getY(), this.getZ(), this.worldObj, particlecount);
 				return true;
 			}
 		}

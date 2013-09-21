8789b3284baa42ce6f8db775c8e92ddb46277796
Guilherme  Espada <porcariadagata@gmail.com>
2013-09-22 00:02:49 +0100

    Fix typo.

--- common/logisticspipes/commands/commands/NBTDebugCommand.java
+++ common/logisticspipes/commands/commands/NBTDebugCommand.java
@@ -2,7 +2,7 @@ package logisticspipes.commands.commands;
 
 import logisticspipes.commands.abstracts.ICommandHandler;
 import logisticspipes.network.PacketHandler;
-import logisticspipes.network.packets.ActivatNBTDebug;
+import logisticspipes.network.packets.ActivateNBTDebug;
 import logisticspipes.proxy.MainProxy;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.ChatMessageComponent;
@@ -28,6 +28,6 @@ public class NBTDebugCommand implements ICommandHandler {
 	@Override
 	public void executeCommand(ICommandSender sender, String[] args) {
 		sender.sendChatToPlayer(ChatMessageComponent.createFromText("Trying to Enable NBTDebug"));
-    	MainProxy.sendPacketToPlayer(PacketHandler.getPacket(ActivatNBTDebug.class), (Player)sender);
+    	MainProxy.sendPacketToPlayer(PacketHandler.getPacket(ActivateNBTDebug.class), (Player)sender);
 	}
 }

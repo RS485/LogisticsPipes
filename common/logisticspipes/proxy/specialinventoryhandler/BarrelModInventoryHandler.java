b90d6b85ff511810e590c68e28ce991cd8e7afd3
Alex <alex.orzechowski@hotmail.ca>
2014-02-04 00:27:02 -0500

    Update
    Updated how some of the stuff works, this no longer edits the item instance directly, it now uses the methods that were made for this kind of purpose.
    
    I know how my own mod works...

--- common/logisticspipes/proxy/specialinventoryhandler/BarrelModInventoryHandler.java
+++ common/logisticspipes/proxy/specialinventoryhandler/BarrelModInventoryHandler.java
@@ -1,57 +1,79 @@
 package logisticspipes.proxy.specialinventoryhandler;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeSet;
 
 import logisticspipes.utils.item.ItemIdentifier;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.ForgeDirection;
+import cpw.mods.fml.common.Loader;
 
+/*
+ * This class was last edited on Feb-04-2014 by need4speed402 the creator of 'The Barrels Mod'
+ * 
+ * DON'T EVER EDIT THIS CLASS -- IF I DO MAKE A CHANGE TO MY MOD, I'LL EDIT THIS CLASS MYSELF
+ */
 public class BarrelModInventoryHandler extends SpecialInventoryHandler {
+	private static final Class tileEntityClass;
+	private static final Method getStackLimit;
+	
+	private static final Method getItem, setItem;
+	
+	private static final Method getModeForSide;
 
-	private static Class <? extends Object> barrelClass;
-	private static Method getStackLimit;
-	private static Field item;
-	private static Method getModeForSide;
-	private static Method inventoryChanged;
-
-	private final TileEntity _tile;
-	private final boolean _hideOnePerStack;
+	static{
+		//Learn this trick: when initializing static fields, do what I did here, it's good practice!
+		//None of that non-static init bs.
+		
+		Class XtileEntityClass = null;
+		Method XgetStackLimit = null, XgetItem = null, XsetItem = null, XgetModeForSide = null;
+		
+		if (Loader.isModLoaded("barrels")){
+			try {
+				XtileEntityClass = Class.forName("need4speed402.mods.barrels.TileEntityBarrel");
+				
+				XgetStackLimit = XtileEntityClass.getMethod("getStackLimit");
+				
+				XgetItem = XtileEntityClass.getMethod("getItem");
+				XsetItem = XtileEntityClass.getMethod("setItem", ItemStack.class);
+				
+				XgetModeForSide = XtileEntityClass.getMethod("getModeForSide", int.class);
+			} catch (Exception e) {
+				XtileEntityClass = null;
+			}
+		}
+		
+		tileEntityClass = XtileEntityClass;
+		getStackLimit = XgetStackLimit;
+		getItem = XgetItem;
+		setItem = XsetItem;
+		getModeForSide = XgetModeForSide;
+	}
+	
+	private final TileEntity tile;
+	private final int hide;
 
 	private BarrelModInventoryHandler(TileEntity tile, boolean hideOnePerStack, boolean hideOne, int cropStart, int cropEnd) {
-		_tile = tile;
-		_hideOnePerStack = hideOnePerStack || hideOne;
+		this.tile = tile;
+		this.hide = hideOnePerStack || hideOne ? 1 : 0;
 	}
 
 	public BarrelModInventoryHandler() {
-		_tile = null;
-		_hideOnePerStack = false;
+		this.tile = null;
+		this.hide = 0;
 	}
-
+	
 	@Override
 	public boolean init() {
-		try {
-			barrelClass = Class.forName("need4speed402.mods.barrels.TileEntityBarrel");
-			getStackLimit = barrelClass.getDeclaredMethod("getStackLimit", new Class[]{});
-			item = barrelClass.getDeclaredField("item");
-			item.setAccessible(true);
-			getModeForSide = barrelClass.getDeclaredMethod("getModeForSide", new Class[]{int.class});
-			inventoryChanged = barrelClass.getDeclaredMethod("inventoryChanged");
-			return true;
-		} catch(Exception e) {
-			return false;
-		}
+		return tileEntityClass != null;
 	}
 
 	@Override
 	public boolean isType(TileEntity tile) {
-		return barrelClass.isAssignableFrom(tile.getClass());
+		return tileEntityClass.isAssignableFrom(tile.getClass());
 	}
 
 	@Override
@@ -59,137 +81,107 @@ public class BarrelModInventoryHandler extends SpecialInventoryHandler {
 		return new BarrelModInventoryHandler(tile, hideOnePerStack, hideOne, cropStart, cropEnd);
 	}
 
-
+	private ItemStack match (ItemIdentifier iden, ItemStack stack){
+		if (stack == null){
+			return null;
+		}
+		
+		return ItemIdentifier.get(stack) == iden ? stack : null;
+	}
+	
 	@Override
 	public int itemCount(ItemIdentifier itemIdent) {
 		try {
-			ItemStack itemStack = (ItemStack) item.get(_tile);
-			if(itemStack != null) {
-				if(ItemIdentifier.get(itemStack) == itemIdent) {
-					int value = itemStack.stackSize;
-					return value - (_hideOnePerStack?1:0);
-				}
+			ItemStack stack = (ItemStack) getItem.invoke(this.tile);
+			
+			if (this.match(itemIdent, stack) != null) {
+				return stack.stackSize - this.hide;
 			}
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
+		} catch (Exception e) {}
+		
 		return 0;
 	}
 
 	@Override
+	public ItemStack getSingleItem(ItemIdentifier itemIdent) {
+		return this.getMultipleItems(itemIdent, 1);
+	}
+	
+	@Override
 	public ItemStack getMultipleItems(ItemIdentifier itemIdent, int count) {
 		try {
-			ItemStack itemStack = (ItemStack) item.get(_tile);
-			if(itemStack != null) {
-				if(ItemIdentifier.get(itemStack) != itemIdent) return null;
-				int value = itemStack.stackSize;
-				if(value - (_hideOnePerStack?1:0) < count) return null;
-				itemStack.stackSize = value - count;
-				inventoryChanged.invoke(_tile);
-				ItemStack ret = itemStack.copy();
+			ItemStack stack = (ItemStack) getItem.invoke(this.tile);
+			ItemStack match = this.match(itemIdent, stack);
+			
+			if (stack != null && match != null) {
+				if (stack.stackSize - this.hide < count){
+					return null;
+				}
+				
+				stack.stackSize -= count;
+				
+				setItem.invoke(this.tile, stack);
+				
+				ItemStack ret = match.copy();
 				ret.stackSize = count;
 				return ret;
 			}
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
+		} catch (Exception e) {}
+		
 		return null;
 	}
 
 	@Override
 	public Set<ItemIdentifier> getItems() {
 		Set<ItemIdentifier> result = new TreeSet<ItemIdentifier>();
+		
 		try {
-			ItemStack itemStack = (ItemStack) item.get(_tile);
-			if(itemStack != null) {
-				result.add(ItemIdentifier.get(itemStack));
+			ItemStack stack = (ItemStack) getItem.invoke(this.tile);
+			
+			if (stack != null){
+				result.add(ItemIdentifier.get(stack));
 			}
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} 
+		} catch (Exception e) {}
+		
 		return result;
 	}
 	
 	@Override
 	public HashMap<ItemIdentifier, Integer> getItemsAndCount() {
-		HashMap<ItemIdentifier, Integer> map = new HashMap<ItemIdentifier, Integer>();
+		HashMap<ItemIdentifier, Integer> result = new HashMap<ItemIdentifier, Integer>();
+		
 		try {
-			ItemStack itemStack = (ItemStack) item.get(_tile);
-			if(itemStack != null) {
-				int value = itemStack.stackSize;
-				map.put(ItemIdentifier.get(itemStack), value - (_hideOnePerStack?1:0));
+			ItemStack stack = (ItemStack) getItem.invoke(this.tile);
+			
+			if (stack != null){
+				result.put(ItemIdentifier.get(stack), stack.stackSize - this.hide);
 			}
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-		return map;
-	}
-
-	@Override
-	public ItemStack getSingleItem(ItemIdentifier itemIdent) {
-		try {
-			ItemStack itemStack = (ItemStack) item.get(_tile);
-			if(itemStack != null) {
-				if(ItemIdentifier.get(itemStack) != itemIdent) return null;
-				int value = itemStack.stackSize;
-				if(value > (_hideOnePerStack?1:0)) {
-					itemStack.stackSize = value - 1;
-					inventoryChanged.invoke(_tile);
-					ItemStack ret = itemStack.copy();
-					ret.stackSize = 1;
-					return ret;
-				}
-			}
-			return null;
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		return null;
+		} catch (Exception e) {}
+		
+		return result;
 	}
 
 	@Override
 	public boolean containsItem(ItemIdentifier itemIdent) {
 		try {
-			ItemStack itemStack = (ItemStack) item.get(_tile);
-			if(itemStack != null) {
-				return ItemIdentifier.get(itemStack) == itemIdent;
-			}
-			return false;
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
+			ItemStack stack = (ItemStack) getItem.invoke(this.tile);
+			
+			return this.match(itemIdent, stack) != null;
+		} catch (Exception e) {}
+		
 		return false;
 	}
 
 	@Override
 	public boolean containsUndamagedItem(ItemIdentifier itemIdent) {
-		try {
-			ItemStack itemStack = (ItemStack) item.get(_tile);
-			if(itemStack != null) {
-				return ItemIdentifier.getUndamaged(itemStack) == itemIdent;
+		try{
+			ItemStack stack = (ItemStack) getItem.invoke(this.tile);
+			
+			if (stack != null){
+				return ItemIdentifier.getUndamaged(stack) == itemIdent;
 			}
-			return false;
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
+		}catch (Exception e){}
+		
 		return false;
 	}
 
@@ -197,27 +189,24 @@ public class BarrelModInventoryHandler extends SpecialInventoryHandler {
 	public int roomForItem(ItemIdentifier item) {
 		return roomForItem(item, 0);
 	}
+	
 	@Override
 	public int roomForItem(ItemIdentifier itemIdent, int count) {
 		try {
-			ItemStack itemStack = (ItemStack) item.get(_tile);
-			int max = (Integer) getStackLimit.invoke(_tile, new Object[]{});
-			if(itemStack != null) {
-				if(ItemIdentifier.get(itemStack) != itemIdent) return 0;
-				max *= itemStack.getMaxStackSize();
-				int value = itemStack.stackSize;
-				return max - value;
-			} else {
-				max *= itemIdent.makeNormalStack(1).getMaxStackSize();
-				return max;
+			ItemStack stack = (ItemStack) getItem.invoke(this.tile);
+			int max = (Integer) getStackLimit.invoke(this.tile);
+			
+			if (stack != null) {
+				if (this.match(itemIdent, stack) == null){
+					return 0;
+				}
+				
+				return max * stack.getMaxStackSize() - stack.stackSize;
+			}else{
+				return max * itemIdent.makeNormalStack(1).getMaxStackSize();
 			}
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
+		} catch (Exception e) {}
+		
 		return 0;
 	}
 
@@ -225,39 +214,38 @@ public class BarrelModInventoryHandler extends SpecialInventoryHandler {
 	public ItemStack add(ItemStack stack, ForgeDirection from, boolean doAdd) {
 		ItemStack st = stack.copy();
 		st.stackSize = 0;
-		if(from == ForgeDirection.UNKNOWN) return st;
+		if (from == ForgeDirection.UNKNOWN){
+			return st;
+		}
+		
 		try {
-			byte mode = (Byte) getModeForSide.invoke(_tile, from.ordinal());
-			if(mode != 2 && mode != 3) return st;
-			ItemStack itemStack = (ItemStack) item.get(_tile);
-			if(itemStack == null) {
-				st.stackSize = stack.stackSize;
-				if(doAdd) {
-					ItemStack tst = stack.copy();
-					if(tst.stackTagCompound != null && tst.stackTagCompound.getName().equals("")) {
-						tst.stackTagCompound.setName("tag");
-					}
-					((IInventory)_tile).setInventorySlotContents(0, tst);
+			Enum mode = (Enum) getModeForSide.invoke(this.tile, from.ordinal());
+			if (mode.name().equals("UNUSED") || mode.name().equals("OUT")){
+				return st;
+			}
+			
+			ItemStack inBarrel = (ItemStack) getItem.invoke(this.tile);
+			int max = (Integer) getStackLimit.invoke(this.tile);
+			
+			if (inBarrel == null) {
+				st.stackSize = Math.min(max * stack.getMaxStackSize(), stack.stackSize);
+				
+				if (doAdd) {
+					setItem.invoke(this.tile, st.copy());
 				}
-			} else {
-				if(ItemIdentifier.get(itemStack) != ItemIdentifier.get(stack)) return st;
-				int max = (Integer) getStackLimit.invoke(_tile, new Object[]{});
-				max *= itemStack.getMaxStackSize();
-				int value = itemStack.stackSize;
-				int room = max - value;
-				st.stackSize = Math.max(Math.min(room, stack.stackSize), 0);
-				if(doAdd && st.stackSize > 0) {
-					itemStack.stackSize = value + st.stackSize;
-					inventoryChanged.invoke(_tile);
+			}else if (this.match(ItemIdentifier.get(stack), inBarrel) != null){
+				int room = max * inBarrel.getMaxStackSize() - inBarrel.stackSize;
+				
+				st.stackSize = Math.min(room, stack.stackSize);
+				
+				if (doAdd) {
+					inBarrel.stackSize += st.stackSize;
+					
+					setItem.invoke(this.tile, inBarrel);
 				}
 			}
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
+		} catch (Exception e) {}
+		
 		return st;
 	}
 
@@ -273,33 +261,30 @@ public class BarrelModInventoryHandler extends SpecialInventoryHandler {
 
 	@Override
 	public ItemStack getStackInSlot(int i) {
-		if(i != 0) return null;
 		try {
-			return (ItemStack) item.get(_tile);
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
+			//as said in the spec, you cannot use this method to get the direct instance of the item in the barrel, it is a re-calculated copy.
+			return (ItemStack) getItem.invoke(this.tile);
+		} catch (Exception e) {}
+		
 		return null;
 	}
 
 	@Override
 	public ItemStack decrStackSize(int i, int j) {
 		try {
-			ItemStack itemStack = (ItemStack) item.get(_tile);
-			int value = itemStack.stackSize;
-			if(value > (_hideOnePerStack?1:0)) {
-				itemStack.stackSize = value - 1;
-				ItemStack ret = itemStack.copy();
+			ItemStack stack = (ItemStack) getItem.invoke(this.tile);
+			
+			if(stack.stackSize > this.hide) {
+				stack.stackSize -= 1;
+				
+				setItem.invoke(this.tile, stack);
+				
+				ItemStack ret = stack.copy();
 				ret.stackSize = 1;
 				return ret;
 			}
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		}
+		} catch (Exception e) {}
+		
 		return null;
 	}
 }

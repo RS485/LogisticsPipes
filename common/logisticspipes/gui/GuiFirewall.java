8b91e5afd2e85eddf6004191f832c88b37e6ab0c
davboecki <dboeken@yahoo.de>
2014-04-25 18:02:07 +0200

    Add translation to firewall GUI.

--- common/logisticspipes/gui/GuiFirewall.java
+++ common/logisticspipes/gui/GuiFirewall.java
@@ -7,10 +7,12 @@ import logisticspipes.utils.gui.DummyContainer;
 import logisticspipes.utils.gui.GuiStringHandlerButton;
 import logisticspipes.utils.gui.GuiStringHandlerButton.StringHandler;
 import logisticspipes.utils.gui.KraphtBaseGuiScreen;
+import logisticspipes.utils.string.StringUtil;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.entity.player.EntityPlayer;
 
 public class GuiFirewall extends KraphtBaseGuiScreen {
+	private static final String PREFIX = "gui.firewall.";
 	
 	private PipeItemsFirewall pipe;
 	
@@ -32,11 +34,13 @@ public class GuiFirewall extends KraphtBaseGuiScreen {
 	public void initGui() {
 		super.initGui();
 		buttonList.clear();
-		buttonList.add(new GuiStringHandlerButton(0, width / 2 + 23, height / 2 + 27 - 139, 60 , 20, new StringHandler() {@Override public String getContent() {return pipe.isBlocking() ? "Blocked" : "Allowed";}}));
-		buttonList.add(new GuiStringHandlerButton(1, width / 2 + 23, height / 2 + 60 - 139, 60 , 20, new StringHandler() {@Override public String getContent() {return pipe.isBlockProvider() ? "Blocked" : "Allowed";}}));
-		buttonList.add(new GuiStringHandlerButton(2, width / 2 + 23, height / 2 + 93 - 139, 60 , 20, new StringHandler() {@Override public String getContent() {return pipe.isBlockCrafer() ? "Blocked" : "Allowed";}}));
-		buttonList.add(new GuiStringHandlerButton(3, width / 2 + 23, height / 2 + 126- 139, 60 , 20, new StringHandler() {@Override public String getContent() {return pipe.isBlockSorting() ? "Blocked" : "Allowed";}}));
-		buttonList.add(new GuiStringHandlerButton(4, width / 2 + 23, height / 2 + 160- 139, 60 , 20, new StringHandler() {@Override public String getContent() {return pipe.isBlockPower() ? "Blocked" : "Allowed";}}));
+		final String blocked = StringUtil.translate(PREFIX + "Blocked");
+		final String allowed = StringUtil.translate(PREFIX + "Allowed");
+		buttonList.add(new GuiStringHandlerButton(0, width / 2 + 23, height / 2 + 27 - 139, 60 , 20, new StringHandler() {@Override public String getContent() {return pipe.isBlocking() ? blocked : allowed;}}));
+		buttonList.add(new GuiStringHandlerButton(1, width / 2 + 23, height / 2 + 60 - 139, 60 , 20, new StringHandler() {@Override public String getContent() {return pipe.isBlockProvider() ? blocked : allowed;}}));
+		buttonList.add(new GuiStringHandlerButton(2, width / 2 + 23, height / 2 + 93 - 139, 60 , 20, new StringHandler() {@Override public String getContent() {return pipe.isBlockCrafer() ? blocked : allowed;}}));
+		buttonList.add(new GuiStringHandlerButton(3, width / 2 + 23, height / 2 + 126- 139, 60 , 20, new StringHandler() {@Override public String getContent() {return pipe.isBlockSorting() ? blocked : allowed;}}));
+		buttonList.add(new GuiStringHandlerButton(4, width / 2 + 23, height / 2 + 160- 139, 60 , 20, new StringHandler() {@Override public String getContent() {return pipe.isBlockPower() ? blocked : allowed;}}));
 	}
 
 	@Override
@@ -71,17 +75,17 @@ public class GuiFirewall extends KraphtBaseGuiScreen {
 	protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3) {
 		BasicGuiHelper.drawGuiBackGround(mc, guiLeft, guiTop, right, bottom, zLevel, true);
 		BasicGuiHelper.drawPlayerInventoryBackground(mc, guiLeft + 33, guiTop + 175);
-		mc.fontRenderer.drawString("Firewall", guiLeft + 45, guiTop + 8, 0x404040);
-		mc.fontRenderer.drawString("Filter:", guiLeft + 14, guiTop + 28, 0x404040);
+		mc.fontRenderer.drawString(StringUtil.translate(PREFIX + "Firewall"), guiLeft + 45, guiTop + 8, 0x404040);
+		mc.fontRenderer.drawString(StringUtil.translate(PREFIX + "Filter") + ":", guiLeft + 14, guiTop + 28, 0x404040);
 		for(int x = 0;x < 6;x++) {
 			for(int y = 0;y < 6;y++) {
 				BasicGuiHelper.drawSlotBackground(mc, guiLeft + x*18 + 16, guiTop + y*18 + 40);
 			}
 		}
-		mc.fontRenderer.drawString("Filtered items are:", guiLeft + 125, guiTop + 8, 0x404040);
-		mc.fontRenderer.drawString("Providing:", guiLeft + 144, guiTop + 41, 0x404040);
-		mc.fontRenderer.drawString("Crafting:", guiLeft + 146, guiTop + 74, 0x404040);
-		mc.fontRenderer.drawString("Sorting:", guiLeft + 150, guiTop + 107, 0x404040);
-		mc.fontRenderer.drawString("Powerflow:", guiLeft + 142, guiTop + 141, 0x404040);
+		mc.fontRenderer.drawString(StringUtil.translate(PREFIX + "Filtereditemsare") + ":", guiLeft + 125, guiTop + 8, 0x404040);
+		mc.fontRenderer.drawString(StringUtil.translate(PREFIX + "Providing") + ":", guiLeft + 144, guiTop + 41, 0x404040);
+		mc.fontRenderer.drawString(StringUtil.translate(PREFIX + "Crafting") + ":", guiLeft + 146, guiTop + 74, 0x404040);
+		mc.fontRenderer.drawString(StringUtil.translate(PREFIX + "Sorting") + ":", guiLeft + 150, guiTop + 107, 0x404040);
+		mc.fontRenderer.drawString(StringUtil.translate(PREFIX + "Powerflow") + ":", guiLeft + 142, guiTop + 141, 0x404040);
 	}
 }

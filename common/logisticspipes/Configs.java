23175acb59a7331efd7d01346160e3b283f1448b
Guilherme Espada <porcariadagata@gmail.com>
2014-05-06 19:27:26 +0100

    Purge ids from configs.

--- common/logisticspipes/Configs.java
+++ common/logisticspipes/Configs.java
@@ -12,55 +12,6 @@ public class Configs {
 	public static final String CATEGORY_MULTITHREAD = "multithread";
 	public static final String CATEGORY_DEBUG 		= "debug";
 
-	// Ids
-	public static int ITEM_BROKEN_ID = 6863;
-	public static int ITEM_LIQUID_CONTAINER_ID = 6864;
-	public static int ITEM_UPGRADE_MANAGER_ID = 6865;
-	public static int ITEM_UPGRADE_ID = 6866;
-	public static int ITEM_PARTS_ID = 6867;
-	public static int ITEM_HUD_ID = 6868;
-	public static int ITEM_CARD_ID = 6869;
-	public static int ITEM_DISK_ID = 6870;
-	public static int ITEM_MODULE_ID = 6871;
-	public static int LOGISTICSREMOTEORDERER_ID = 6872;
-	public static int LOGISTICSNETWORKMONITOR_ID = 6873;
-	public static int LOGISTICSPIPE_BASIC_ID = 6874;
-	public static int LOGISTICSPIPE_REQUEST_ID = 6875;
-	public static int LOGISTICSPIPE_PROVIDER_ID = 6876;
-	public static int LOGISTICSPIPE_CRAFTING_ID = 6877;
-	public static int LOGISTICSPIPE_SATELLITE_ID = 6878;
-	public static int LOGISTICSPIPE_SUPPLIER_ID = 6879;
-	//Free ID from Old Builder Supplier = 6880;
-	public static int LOGISTICSPIPE_CHASSI1_ID = 6881;
-	public static int LOGISTICSPIPE_CHASSI2_ID = 6882;
-	public static int LOGISTICSPIPE_CHASSI3_ID = 6883;
-	public static int LOGISTICSPIPE_CHASSI4_ID = 6884;
-	public static int LOGISTICSPIPE_CHASSI5_ID = 6885;
-	public static int LOGISTICSPIPE_LIQUIDSUPPLIER_ID = 6886;
-	public static int LOGISTICSPIPE_CRAFTING_MK2_ID = 6887;
-	public static int LOGISTICSPIPE_REQUEST_MK2_ID = 6888;
-	public static int LOGISTICSPIPE_REMOTE_ORDERER_ID = 6889;
-	public static int LOGISTICSPIPE_PROVIDER_MK2_ID = 6890;
-	public static int LOGISTICSPIPE_APIARIST_ANALYSER_ID = 6891;
-	public static int LOGISTICSPIPE_APIARIST_SINK_ID = 6892;
-	public static int LOGISTICSPIPE_INVSYSCON_ID = 6893;
-	public static int LOGISTICSPIPE_ENTRANCE_ID = 6894;
-	public static int LOGISTICSPIPE_DESTINATION_ID = 6895;
-	public static int LOGISTICSPIPE_CRAFTING_MK3_ID = 6896;
-	public static int LOGISTICSPIPE_FIREWALL_ID = 6897;
-	public static int LOGISTICSPIPE_REQUEST_TABLE_ID = 6898;
-
-	public static int LOGISTICSPIPE_LIQUID_CONNECTOR = 6901;
-	public static int LOGISTICSPIPE_LIQUID_BASIC = 6902;
-	public static int LOGISTICSPIPE_LIQUID_INSERTION = 6903;
-	public static int LOGISTICSPIPE_LIQUID_PROVIDER = 6904;
-	public static int LOGISTICSPIPE_LIQUID_REQUEST = 6905;
-	public static int LOGISTICSPIPE_LIQUID_EXTRACTOR = 6906;
-	public static int LOGISTICSPIPE_LIQUID_SATELLITE = 6907;
-	public static int LOGISTICSPIPE_LIQUID_SUPPLIER_MK2 = 6908;
-
-	public static int LOGISTICSCRAFTINGSIGNCREATOR_ID = 6900;
-
 	private static Configuration CONFIGURATION;
 
 	// Configrables
@@ -83,9 +34,6 @@ public class Configs {
 
 	// GuiOrderer Popup setting
 	public static boolean DISPLAY_POPUP = true;
-
-	// BlockID
-	public static int LOGISTICS_SOLID_BLOCK_ID = 1101;
 
 	// MultiThread
 	public static int MULTI_THREAD_NUMBER = 4;
@@ -117,143 +65,6 @@ public class Configs {
 			throw new RuntimeException(
 					"Old config, please remove it and manually reconfigure LogisticPipes");
 		}
-
-		LOGISTICSNETWORKMONITOR_ID = CONFIGURATION.getItem(
-				"logisticsNetworkMonitor.id", LOGISTICSNETWORKMONITOR_ID,
-				"The item id for the network monitor").getInt();
-		LOGISTICSREMOTEORDERER_ID = CONFIGURATION.getItem(
-				"logisticsRemoteOrderer.id", LOGISTICSREMOTEORDERER_ID,
-				"The item id for the remote orderer").getInt();
-		ITEM_MODULE_ID = CONFIGURATION.getItem("logisticsModules.id",
-				ITEM_MODULE_ID, "The item id for the modules").getInt();
-		ITEM_UPGRADE_ID = CONFIGURATION.getItem("logisticsUpgrades.id",
-				ITEM_UPGRADE_ID, "The item id for the upgrades").getInt();
-		ITEM_UPGRADE_MANAGER_ID = CONFIGURATION.getItem(
-				"logisticsUpgradeManager.id", ITEM_UPGRADE_MANAGER_ID,
-				"The item id for the upgrade manager").getInt();
-		ITEM_DISK_ID = CONFIGURATION.getItem("logisticsDisk.id", ITEM_DISK_ID,
-				"The item id for the disk").getInt();
-		ITEM_CARD_ID = CONFIGURATION.getItem("logisticItemCard.id",
-				ITEM_CARD_ID, "The item id for the logistics item card")
-				.getInt();
-		ITEM_HUD_ID = CONFIGURATION.getItem("logisticsHUD.id", ITEM_HUD_ID,
-				"The item id for the Logistics HUD glasses").getInt();
-		ITEM_PARTS_ID = CONFIGURATION.getItem("logisticsHUDParts.id",
-				ITEM_PARTS_ID, "The item id for the Logistics item parts")
-				.getInt();
-		ITEM_BROKEN_ID = CONFIGURATION.getItem("LogisticsBrokenItem.id",
-				ITEM_BROKEN_ID, "The item id for the logistics broken item")
-				.getInt();
-	
-		ITEM_LIQUID_CONTAINER_ID = CONFIGURATION.getItem(
-				"LogisticsLiquidContainer.id", ITEM_LIQUID_CONTAINER_ID,
-				"The item id for the logistics liquid container").getInt();
-		
-		LOGISTICSPIPE_BASIC_ID = CONFIGURATION.getItem("logisticsPipe.id",
-				LOGISTICSPIPE_BASIC_ID,
-				"The item id for the basic logistics pipe").getInt();
-		LOGISTICSPIPE_REQUEST_ID = CONFIGURATION.getItem(
-				"logisticsPipeRequester.id", LOGISTICSPIPE_REQUEST_ID,
-				"The item id for the requesting logistics pipe").getInt();
-		LOGISTICSPIPE_PROVIDER_ID = CONFIGURATION.getItem(
-				"logisticsPipeProvider.id", LOGISTICSPIPE_PROVIDER_ID,
-				"The item id for the providing logistics pipe").getInt();
-		LOGISTICSPIPE_CRAFTING_ID = CONFIGURATION.getItem(
-				"logisticsPipeCrafting.id", LOGISTICSPIPE_CRAFTING_ID,
-				"The item id for the crafting logistics pipe").getInt();
-		LOGISTICSPIPE_SATELLITE_ID = CONFIGURATION.getItem(
-				"logisticsPipeSatellite.id", LOGISTICSPIPE_SATELLITE_ID,
-				"The item id for the crafting satellite pipe").getInt();
-		LOGISTICSPIPE_SUPPLIER_ID = CONFIGURATION.getItem(
-				"logisticsPipeSupplier.id", LOGISTICSPIPE_SUPPLIER_ID,
-				"The item id for the supplier pipe").getInt();
-		LOGISTICSPIPE_CHASSI1_ID = CONFIGURATION.getItem(
-				"logisticsPipeChassi1.id", LOGISTICSPIPE_CHASSI1_ID,
-				"The item id for the chassi1").getInt();
-		LOGISTICSPIPE_CHASSI2_ID = CONFIGURATION.getItem(
-				"logisticsPipeChassi2.id", LOGISTICSPIPE_CHASSI2_ID,
-				"The item id for the chassi2").getInt();
-		LOGISTICSPIPE_CHASSI3_ID = CONFIGURATION.getItem(
-				"logisticsPipeChassi3.id", LOGISTICSPIPE_CHASSI3_ID,
-				"The item id for the chassi3").getInt();
-		LOGISTICSPIPE_CHASSI4_ID = CONFIGURATION.getItem(
-				"logisticsPipeChassi4.id", LOGISTICSPIPE_CHASSI4_ID,
-				"The item id for the chassi4").getInt();
-		LOGISTICSPIPE_CHASSI5_ID = CONFIGURATION.getItem(
-				"logisticsPipeChassi5.id", LOGISTICSPIPE_CHASSI5_ID,
-				"The item id for the chassi5").getInt();
-		LOGISTICSPIPE_CRAFTING_MK2_ID = CONFIGURATION.getItem(
-				"logisticsPipeCraftingMK2.id", LOGISTICSPIPE_CRAFTING_MK2_ID,
-				"The item id for the crafting logistics pipe MK2").getInt();
-		LOGISTICSPIPE_CRAFTING_MK3_ID = CONFIGURATION.getItem(
-				"logisticsPipeCraftingMK3.id", LOGISTICSPIPE_CRAFTING_MK3_ID,
-				"The item id for the crafting logistics pipe MK3").getInt();
-		LOGISTICSPIPE_REQUEST_MK2_ID = CONFIGURATION.getItem(
-				"logisticsPipeRequesterMK2.id", LOGISTICSPIPE_REQUEST_MK2_ID,
-				"The item id for the requesting logistics pipe MK2").getInt();
-		LOGISTICSPIPE_PROVIDER_MK2_ID = CONFIGURATION.getItem(
-				"logisticsPipeProviderMK2.id", LOGISTICSPIPE_PROVIDER_MK2_ID,
-				"The item id for the provider logistics pipe MK2").getInt();
-		LOGISTICSPIPE_REMOTE_ORDERER_ID = CONFIGURATION.getItem(
-				"logisticsPipeRemoteOrderer.id",
-				LOGISTICSPIPE_REMOTE_ORDERER_ID,
-				"The item id for the remote orderer logistics pipe").getInt();
-		LOGISTICSPIPE_APIARIST_ANALYSER_ID = CONFIGURATION.getItem(
-				"logisticsPipeApiaristAnalyser.id",
-				LOGISTICSPIPE_APIARIST_ANALYSER_ID,
-				"The item id for the apiarist logistics analyser pipe")
-				.getInt();
-		LOGISTICSPIPE_APIARIST_SINK_ID = CONFIGURATION.getItem(
-				"logisticsPipeApiaristSink.id", LOGISTICSPIPE_APIARIST_SINK_ID,
-				"The item id for the apiarist logistics sink pipe").getInt();
-		LOGISTICSPIPE_ENTRANCE_ID = CONFIGURATION.getItem(
-				"logisticEntrance.id", LOGISTICSPIPE_ENTRANCE_ID,
-				"The item id for the logistics system entrance pipe").getInt();
-		LOGISTICSPIPE_DESTINATION_ID = CONFIGURATION.getItem(
-				"logisticDestination.id", LOGISTICSPIPE_DESTINATION_ID,
-				"The item id for the logistics system destination pipe")
-				.getInt();
-		LOGISTICSPIPE_INVSYSCON_ID = CONFIGURATION.getItem(
-				"logisticInvSysCon.id", LOGISTICSPIPE_INVSYSCON_ID,
-				"The item id for the inventory system connector pipe").getInt();
-		LOGISTICS_SOLID_BLOCK_ID = CONFIGURATION.getBlock(
-				"logisticsSolidBlockId", LOGISTICS_SOLID_BLOCK_ID,
-				"The ID of the LogisticsPipes Solid Block").getInt();
-		LOGISTICSPIPE_FIREWALL_ID = CONFIGURATION.getItem(
-				"logisticsPipeFirewall.id", LOGISTICSPIPE_FIREWALL_ID,
-				"The item id for the firewall logistics pipe").getInt();
-
-		LOGISTICSPIPE_LIQUID_CONNECTOR = CONFIGURATION.getItem(
-				"logisticPipeLiquidConnector.id",
-				LOGISTICSPIPE_LIQUID_CONNECTOR,
-				"The item id for the liquid connector pipe.").getInt();
-		LOGISTICSPIPE_LIQUID_BASIC = CONFIGURATION.getItem(
-				"logisticPipeLiquidBasic.id", LOGISTICSPIPE_LIQUID_BASIC,
-				"The item id for the liquid basic pipe.").getInt();
-		LOGISTICSPIPE_LIQUID_INSERTION = CONFIGURATION.getItem(
-				"logisticPipeLiquidInsertion.id",
-				LOGISTICSPIPE_LIQUID_INSERTION,
-				"The item id for the liquid insertion pipe.").getInt();
-		LOGISTICSPIPE_LIQUID_PROVIDER = CONFIGURATION.getItem(
-				"logisticPipeLiquidProvider.id",
-				LOGISTICSPIPE_LIQUID_PROVIDER,
-				"The item id for the liquid provider pipe.").getInt();
-		LOGISTICSPIPE_LIQUID_REQUEST = CONFIGURATION.getItem(
-				"logisticPipeLiquidRequest.id",
-				LOGISTICSPIPE_LIQUID_REQUEST,
-				"The item id for the liquid requestor pipe.").getInt();
-		LOGISTICSPIPE_LIQUID_EXTRACTOR = CONFIGURATION.getItem(
-				"logisticPipeLiquidExtractor.id",
-				LOGISTICSPIPE_LIQUID_EXTRACTOR,
-				"The item id for the liquid extractor pipe.").getInt();
-		LOGISTICSPIPE_LIQUID_SATELLITE = CONFIGURATION.getItem(
-				"logisticPipeLiquidSatellite.id",
-				LOGISTICSPIPE_LIQUID_SATELLITE,
-				"The item id for the liquid satellite pipe.").getInt();
-		LOGISTICSPIPE_LIQUID_SUPPLIER_MK2 = CONFIGURATION.getItem(
-				"logisticPipeLiquidSupplierMk2.id",
-				LOGISTICSPIPE_LIQUID_SUPPLIER_MK2,
-				"The item id for the liquid supplier pipe mk2.").getInt();
 		
 		LOGISTICS_DETECTION_LENGTH = CONFIGURATION
 				.get(Configuration.CATEGORY_GENERAL,
@@ -291,11 +102,6 @@ public class Configs {
 				"Disable the power usage trough LogisticsPipes").getBoolean(
 				false);
 
-		LOGISTICSCRAFTINGSIGNCREATOR_ID = CONFIGURATION.getItem(
-				"logisticsCraftingSignCreator.id",
-				LOGISTICSCRAFTINGSIGNCREATOR_ID,
-				"The item id for the crafting sign creator").getInt();
-
 		LOGISTICS_HUD_RENDER_DISTANCE = CONFIGURATION
 				.get(Configuration.CATEGORY_GENERAL, "HUDRenderDistance",
 						LOGISTICS_HUD_RENDER_DISTANCE,
@@ -309,10 +115,6 @@ public class Configs {
 						"Set the default configuration for the popup of the Orderer Gui. Should it be used?")
 				.getBoolean(false);
 
-		LOGISTICSPIPE_LIQUIDSUPPLIER_ID = CONFIGURATION.getItem(
-				"logisticsPipeLiquidSupplier.id",
-				LOGISTICSPIPE_LIQUIDSUPPLIER_ID,
-				"The item id for the liquid supplier pipe").getInt();
 		MANDATORY_CARPENTER_RECIPES = CONFIGURATION
 				.get(Configuration.CATEGORY_GENERAL,
 						"mandatoryCarpenterRecipes",
@@ -379,10 +181,6 @@ public class Configs {
 								LOGISTICS_CRAFTING_TABLE_POWER_USAGE,
 								"Number of LPower units the Logistics Crafting Table uses per craft.")
 						.getInt(), 0);
-
-		LOGISTICSPIPE_REQUEST_TABLE_ID = CONFIGURATION.getItem(
-				"logisticsPipeRequestTable.id", LOGISTICSPIPE_REQUEST_TABLE_ID,
-				"The item id for the request table").getInt();
 
 		CHECK_FOR_UPDATES = CONFIGURATION
 				.get(Configuration.CATEGORY_GENERAL,

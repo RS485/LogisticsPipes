19932f053550715fed0008f38c13b8a1a0f77d8f
davboecki <dboeken@yahoo.de>
2014-06-25 21:59:03 +0200

    Add dummy files.

---
+++ dummy/org/luaj/vm2/LuaNil.java
@@ -0,0 +1,12 @@
+package org.luaj.vm2;
+
+public class LuaNil extends LuaValue {
+	
+	static final LuaNil _NIL = new LuaNil();
+
+	@Override
+	public int type() {
+		return 0;
+	}
+	
+}
---
+++ dummy/org/luaj/vm2/Varargs.java
@@ -0,0 +1,6 @@
+package org.luaj.vm2;
+
+public abstract class Varargs {
+	abstract public LuaValue arg1();
+	abstract public LuaValue arg( int i );
+}

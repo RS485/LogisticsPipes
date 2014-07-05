RS485/LogisticsPipes

commit 80337ca43a94db91eaa1eadc5270c0a143370d9f

Damage and maxstacksize now ask using a itemStack. Cache masStackSize. Remove some of the ItemIdentifier/ItemStack duplication for methods.

common/logisticspipes/utils/item/ItemIdentifier.java
@@ -165,6 +165,8 @@ public final class ItemIdentifier implements Comparable<ItemIdentifier> {
 	public final FinalNBTTagCompound tag;
 	protected final int uniqueID;
 	
+	private int maxStackSize = 0;
+
 	private ItemIdentifier _IDIgnoringNBT=null;
 	private ItemIdentifier _IDIgnoringDamage=null;
 
@@ -279,17 +281,6 @@ public final class ItemIdentifier implements Comparable<ItemIdentifier> {
 		return get(itemStack.itemID, itemStack.getItemDamage(), itemStack.stackTagCompound);
 	}
 	
-	public static ItemIdentifier getUndamaged(ItemStack itemStack) {
-		if (itemStack == null && allowNullsForTesting){
-			return null;
-		}
-		int itemDamage = 0;
-		if (!Item.itemsList[itemStack.itemID].isDamageable()) {
-			itemDamage = itemStack.getItemDamage();
-		}
-		return get(itemStack.itemID, itemDamage, itemStack.stackTagCompound);
-	}
-
 	public static List<ItemIdentifier> getMatchingNBTIdentifier(int itemID, int itemData) {
 		//inefficient, we'll have to add another map if this becomes a bottleneck
 		ArrayList<ItemIdentifier> resultlist = new ArrayList<ItemIdentifier>(16);
@@ -303,43 +294,6 @@ public final class ItemIdentifier implements Comparable<ItemIdentifier> {
 		keyRefRlock.unlock();
 		return resultlist;
 	}
-
-	public ItemIdentifier getUndamaged() {
-		if(_IDIgnoringDamage==null){
-			if (!Item.itemsList[this.itemID].isDamageable()) {
-				_IDIgnoringDamage = this;
-			} else {
-				_IDIgnoringDamage = get(this.itemID, 0, this.tag);
-			}
-		}
-		return _IDIgnoringDamage;
-	}
-
-	public static ItemIdentifier getIgnoringNBT(ItemStack itemStack) {
-		return get(itemStack.itemID, itemStack.getItemDamage(), null);
-	}
-
-	public ItemIdentifier getIgnoringNBT() {
-		if(this._IDIgnoringNBT==null){
-			_IDIgnoringNBT= get(itemID, itemDamage, null);			
-		}
-		return _IDIgnoringNBT;
-	}
-
-	/*
-	private static boolean tagsequal(NBTTagCompound tag1, NBTTagCompound tag2) {
-		if(tag1 == null && tag2 == null) {
-			return true;
-		}
-		if(tag1 == null) {
-			return false;
-		}
-		if(tag2 == null) {
-			return false;
-		}
-		return tag1.equals(tag2);
-	}
-	*/
 
 	public static void tick() {
 		if(init) return;
@@ -369,6 +323,30 @@ public final class ItemIdentifier implements Comparable<ItemIdentifier> {
 	
 	/* Instance Methods */
 	
+	public ItemIdentifier getUndamaged() {
+		if(_IDIgnoringDamage == null) {
+			if(!unsafeMakeNormalStack(0).isItemStackDamageable()) {
+				_IDIgnoringDamage = this;
+			} else {
+				ItemStack tstack = makeNormalStack(0);
+				tstack.setItemDamage(0);
+				_IDIgnoringDamage = get(tstack);
+			}
+		}
+		return _IDIgnoringDamage;
+	}
+
+	public ItemIdentifier getIgnoringNBT() {
+		if(_IDIgnoringNBT == null) {
+			if(tag == null) {
+				_IDIgnoringNBT = this;
+			} else {
+				_IDIgnoringNBT = get(itemID, itemDamage, null);
+			}
+		}
+		return _IDIgnoringNBT;
+	}
+
 	public String getDebugName() {
 		if (Item.itemsList[itemID] != null)	{
 			return Item.itemsList[itemID].getUnlocalizedName() + "(ID: " + itemID + ", Damage: " + itemDamage + ")";
@@ -409,7 +387,7 @@ public final class ItemIdentifier implements Comparable<ItemIdentifier> {
 	
 	public String getFriendlyName() {
 		if (Item.itemsList[itemID] != null) {
-			return getName(itemID,this.unsafeMakeNormalStack(1));
+			return getName(itemID,this.unsafeMakeNormalStack(0));
 		}
 		return "<Item name not found>";
 	}
@@ -456,17 +434,16 @@ public final class ItemIdentifier implements Comparable<ItemIdentifier> {
 	}
 	
 	public int getMaxStackSize() {
-		if(Item.itemsList[this.itemID].isDamageable() && this.itemDamage > 0) {
-			return 1;
+		if(maxStackSize == 0) {
+			ItemStack tstack = this.unsafeMakeNormalStack(0);
+			int tstacksize = tstack.getMaxStackSize();
+			if(tstack.isItemStackDamageable() && tstack.isItemDamaged()) {
+				tstacksize = 1;
+			}
+			tstacksize = Math.max(1, Math.min(64, tstacksize));
+			maxStackSize = tstacksize;
 		}
-		int limit = Item.itemsList[this.itemID].getItemStackLimit();
-		return limit < 64 ? limit : 64;
-	}
-	
-	public boolean fuzzyMatch(ItemStack stack) {
-		if(stack.itemID != this.itemID) return false;
-		if(stack.getItemDamage() != this.itemDamage) return false;
-		return true;
+		return maxStackSize;
 	}
 	
 	public String getNBTTagCompoundName() {
@@ -657,12 +634,16 @@ public final class ItemIdentifier implements Comparable<ItemIdentifier> {
 		return this.itemID == item.itemID && (item.isDamagable() ? true : this.itemDamage == item.itemDamage);
 	}
 
+	public boolean equalsWithoutNBT(ItemStack stack) {
+		return this.itemID == stack.itemID && this.itemDamage == stack.getItemDamage();
+	}
+
 	public boolean equalsWithoutNBT(ItemIdentifier item) {
 		return this.itemID == item.itemID && this.itemDamage == item.itemDamage;
 	}
 
 	public boolean isDamagable() {
-		return this.makeNormalStack(0).getItem().isDamageable();
+		return this.getUndamaged() == this;
 	}
 
 	private static void checkNBTbadness(ItemIdentifier item, NBTBase nbt) {

common/logisticspipes/modules/ModuleCrafter.java
@@ -1281,7 +1281,7 @@ public class ModuleCrafter extends LogisticsGuiModule implements ICraftItems, IH
 			ItemStack stack = stacks[0];
 			if(stack.stackSize == 0) break;
 			if(retstack == null) {
-				if(!wanteditem.fuzzyMatch(stack)) break;
+				if(!wanteditem.equalsWithoutNBT(stack)) break;
 			} else {
 				if(!retstack.isItemEqual(stack)) break;
 				if(!ItemStack.areItemStackTagsEqual(retstack, stack)) break;
@@ -1315,14 +1315,14 @@ public class ModuleCrafter extends LogisticsGuiModule implements ICraftItems, IH
 			for(int i=0;i<filter.getSizeInventory() && i < filterInvLimit;i++) {
 				ItemIdentifierStack identStack = filter.getIDStackInSlot(i);
 				if(identStack == null) continue;
-				if(identStack.getItem().fuzzyMatch(stack)) return null;
+				if(identStack.getItem().equalsWithoutNBT(stack)) return null;
 			}
 		} else {
 			boolean found = false;
 			for(int i=0;i<filter.getSizeInventory() && i < filterInvLimit;i++) {
 				ItemIdentifierStack identStack = filter.getIDStackInSlot(i);
 				if(identStack == null) continue;
-				if(identStack.getItem().fuzzyMatch(stack)) {
+				if(identStack.getItem().equalsWithoutNBT(stack)) {
 					found = true;
 					break;
 				}
@@ -1363,7 +1363,7 @@ public class ModuleCrafter extends LogisticsGuiModule implements ICraftItems, IH
 				for(int i=0;i<filter.getSizeInventory() && i < filterInvLimit;i++) {
 					ItemIdentifierStack identStack = filter.getIDStackInSlot(i);
 					if(identStack == null) continue;
-					if(identStack.getItem().fuzzyMatch(item.makeNormalStack(1))) {
+					if(identStack.getItem().equalsWithoutNBT(item)) {
 						found = true;
 						break;
 					}
@@ -1376,7 +1376,7 @@ public class ModuleCrafter extends LogisticsGuiModule implements ICraftItems, IH
 				for(int i=0;i<filter.getSizeInventory() && i < filterInvLimit;i++) {
 					ItemIdentifierStack identStack = filter.getIDStackInSlot(i);
 					if(identStack == null) continue;
-					if(identStack.getItem().fuzzyMatch(item.makeNormalStack(1))) {
+					if(identStack.getItem().equalsWithoutNBT(item)) {
 						found = true;
 						break;
 					}
@@ -1405,7 +1405,7 @@ public class ModuleCrafter extends LogisticsGuiModule implements ICraftItems, IH
 			ItemStack stack = tile.getOutput(wanteditem, _service);
 			if(stack == null || stack.stackSize == 0) break;
 			if(retstack == null) {
-				if(!wanteditem.fuzzyMatch(stack)) break;
+				if(!wanteditem.equalsWithoutNBT(stack)) break;
 			} else {
 				if(!retstack.isItemEqual(stack)) break;
 				if(!ItemStack.areItemStackTagsEqual(retstack, stack)) break;

common/logisticspipes/proxy/specialinventoryhandler/AEInterfaceInventoryHandler.java
@@ -103,7 +103,7 @@ public class AEInterfaceInventoryHandler extends SpecialInventoryHandler {
 	@Override
 	public boolean containsUndamagedItem(ItemIdentifier item) {
 		for(ItemStack items: _tile.apiGetNetworkContents()) {
-			ItemIdentifier ident = ItemIdentifier.getUndamaged(items);
+			ItemIdentifier ident = ItemIdentifier.get(items).getUndamaged();
 			if(ident.equals(item)) {
 				return true;
 			}

common/logisticspipes/proxy/specialinventoryhandler/BarrelInventoryHandler.java
@@ -185,7 +185,7 @@ public class BarrelInventoryHandler extends SpecialInventoryHandler {
 		try {
 			ItemStack itemStack = (ItemStack) item.get(_tile);
 			if(itemStack != null) {
-				return ItemIdentifier.getUndamaged(itemStack).equals(itemIdent);
+				return ItemIdentifier.get(itemStack).getUndamaged().equals(itemIdent);
 			}
 			return false;
 		} catch (IllegalArgumentException e) {

common/logisticspipes/proxy/specialinventoryhandler/CrateInventoryHandler.java
@@ -163,7 +163,7 @@ public class CrateInventoryHandler extends SpecialInventoryHandler {
 			int numitems = (Integer) getNumItems.invoke(cratePileData, new Object[]{});
 			for(int i = 0; i < numitems; i++) {
 				ItemStack itemStack = (ItemStack) getItemStack.invoke(cratePileData, new Object[]{i});
-				ItemIdentifier itemId = ItemIdentifier.getUndamaged(itemStack);
+				ItemIdentifier itemId = ItemIdentifier.get(itemStack).getUndamaged();
 				if(itemId.equals(itemIdent)) {
 					return true;
 				}

common/logisticspipes/proxy/specialinventoryhandler/DSUInventoryHandler.java
@@ -101,7 +101,7 @@ public class DSUInventoryHandler extends SpecialInventoryHandler {
 	@Override
 	public boolean containsUndamagedItem(ItemIdentifier itemIdent) {
 		ItemStack items = _tile.getStoredItemType();
-		if(items != null && ItemIdentifier.getUndamaged(items).equals(itemIdent))
+		if(items != null && ItemIdentifier.get(items).getUndamaged().equals(itemIdent))
 			return true;
 		return false;		
 	}

common/logisticspipes/proxy/specialinventoryhandler/JABBAInventoryHandler.java
@@ -110,7 +110,7 @@ public class JABBAInventoryHandler extends SpecialInventoryHandler {
 	@Override
 	public boolean containsUndamagedItem(ItemIdentifier itemIdent) {
 		ItemStack items = _storage.getStoredItemType();
-		if(items != null && ItemIdentifier.getUndamaged(items).equals(itemIdent))
+		if(items != null && ItemIdentifier.get(items).getUndamaged().equals(itemIdent))
 			return true;
 		return false;		
 	}

common/logisticspipes/utils/FuzzyInventoryUtil.java
@@ -19,7 +19,7 @@ public class FuzzyInventoryUtil extends InventoryUtil {
 		for (int i =0; i < _inventory.getSizeInventory(); i++){
 			ItemStack stack = _inventory.getStackInSlot(i);
 			if (stack == null) continue;
-				items.add(ItemIdentifier.getIgnoringNBT(stack));
+				items.add(ItemIdentifier.get(stack).getIgnoringNBT());
 		}
 		return items;
 	}

commit 72bd26ff66d9adf5a235b479e62c8f88210bbe9a

Part of 80337ca43a94db91eaa1eadc5270c0a143370d9f broke matching damaged items as crafting materials, fix that. Fixes #399

common/logisticspipes/utils/item/ItemIdentifier.java
@@ -609,7 +609,7 @@ public final class ItemIdentifier implements Comparable<ItemIdentifier> {
 	}
 
 	public boolean isDamageable() {
-		return this.getUndamaged() == this;
+		return this.unsafeMakeNormalStack(0).isItemStackDamageable();
 	}
 
 	private static void checkNBTbadness(ItemIdentifier item, NBTBase nbt) {
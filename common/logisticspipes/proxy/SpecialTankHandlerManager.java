08d718b9405cedb0da9e26683029e496e0b586ee
davboecki <dboeken@yahoo.de>
2014-08-18 19:10:05 +0200

    Remove unused mod integration.

--- common/logisticspipes/proxy/SpecialTankHandlerManager.java
+++ common/logisticspipes/proxy/SpecialTankHandlerManager.java
@@ -1,15 +1,13 @@
 package logisticspipes.proxy;
 
 import logisticspipes.proxy.specialtankhandler.BuildCraftTankHandler;
-import logisticspipes.proxy.specialtankhandler.ECInterfaceHandler;
 import cpw.mods.fml.common.Loader;
 
 public class SpecialTankHandlerManager {
 
 	public static void load() {
-		SimpleServiceLocator.specialTankHandler.registerHandler(new BuildCraftTankHandler());
-		if(Loader.isModLoaded("extracells")) {
-			SimpleServiceLocator.specialTankHandler.registerHandler(new ECInterfaceHandler());
+		if(Loader.isModLoaded("BuildCraft|Factory")) {
+			SimpleServiceLocator.specialTankHandler.registerHandler(new BuildCraftTankHandler());
 		}
 	}
 }

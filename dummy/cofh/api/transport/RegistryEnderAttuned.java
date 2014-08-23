c4c8119e89d831960cb2d260fed082042c287b8b
davboecki <dboeken@yahoo.de>
2014-08-23 13:55:46 +0200

    Add previously ignored files.

---
+++ dummy/cofh/api/transport/RegistryEnderAttuned.java
@@ -0,0 +1,9 @@
+package cofh.api.transport;
+
+import java.util.List;
+
+public final class RegistryEnderAttuned {
+	public static List<IEnderItemHandler> getLinkedItemOutputs(IEnderItemHandler theAttuned) {
+		return null;
+	}
+}

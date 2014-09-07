42c13079d8172372b30802510be746f5d9e29310
ArtForz <artforz@artforz.de>
2014-09-07 02:44:04 +0200

    Add multi-name support to OreDictSink GUI Closes #438

--- common/logisticspipes/gui/modules/GuiOreDictItemSink.java
+++ common/logisticspipes/gui/modules/GuiOreDictItemSink.java
@@ -1,6 +1,7 @@
 package logisticspipes.gui.modules;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import logisticspipes.modules.ModuleOreDictItemSink;
 import logisticspipes.utils.gui.BasicGuiHelper;
@@ -19,6 +20,9 @@ public class GuiOreDictItemSink extends ModuleBaseGui {
 
 	private int mouseX = 0;
 	private int mouseY = 0;
+
+	private List<String> unsunkNames = new ArrayList<String>();
+	private int currentOffset = 0;
 
 	public GuiOreDictItemSink(IInventory playerInventory, ModuleOreDictItemSink itemSink) {
 		super(null, itemSink);
@@ -42,27 +46,18 @@ public class GuiOreDictItemSink extends ModuleBaseGui {
 	public void initGui() {
 		super.initGui();
 		this.buttonList.clear();
-		this.buttonList.add(new SmallGuiButton(0, guiLeft + 38, guiTop + 18, 50, 10, "Add"));
-		this.buttonList.add(new SmallGuiButton(1, guiLeft + 107, guiTop + 18, 50, 10, "Remove"));
-		((GuiButton)buttonList.get(0)).enabled = false;
-		((GuiButton)buttonList.get(1)).enabled = false;
+		this.buttonList.add(new SmallGuiButton(0, guiLeft + 159, guiTop + 5, 10, 10, ""));
+		this.buttonList.add(new SmallGuiButton(1, guiLeft + 159, guiTop + 17, 10, 10, ""));
+		((GuiButton)buttonList.get(0)).enabled = true;
+		((GuiButton)buttonList.get(1)).enabled = true;
 	}
 
 	@Override
 	protected void actionPerformed(GuiButton par1GuiButton) {
 		if(par1GuiButton.id == 0) {
-			if(tmpInv.getStackInSlot(0) != null) {
-				String oreName = getOreName(tmpInv.getStackInSlot(0));
-				if(!oreName.equals("") && !_itemSink.oreList.contains(oreName)) {
-					_itemSink.oreList.add(oreName);
-					_itemSink.OreListChanged();
-				}
-			}
+			currentOffset -= 1;
 		} else if(par1GuiButton.id == 1) {
-			if(tmpInv.getStackInSlot(0) != null && _itemSink.oreList.contains(getOreName(tmpInv.getStackInSlot(0)))) {
-				_itemSink.oreList.remove(getOreName(tmpInv.getStackInSlot(0)));
-				_itemSink.OreListChanged();
-			}
+			currentOffset += 1;
 		} else {
 			super.actionPerformed(par1GuiButton);
 		}
@@ -81,31 +76,50 @@ public class GuiOreDictItemSink extends ModuleBaseGui {
 
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3) {
+		int pointerX = var2 - guiLeft;
+		int pointerY = var3 - guiTop;
 		BasicGuiHelper.drawGuiBackGround(mc, guiLeft, guiTop, right, bottom, zLevel, true);
 		BasicGuiHelper.drawPlayerInventoryBackground(mc, guiLeft + 7, guiTop + 126);
 		BasicGuiHelper.drawSlotBackground(mc, guiLeft + 6, guiTop + 7);
-		BasicGuiHelper.drawRect(guiLeft + 26, guiTop + 5, guiLeft + 169, guiTop + 17, 0xff808080);
+
 		if(tmpInv.getStackInSlot(0) != null) {
-			String oreName = getOreName(tmpInv.getStackInSlot(0));
-			mc.fontRenderer.drawString(oreName, guiLeft + 28, guiTop + 7, 0x404040);
-			if(_itemSink.oreList.contains(oreName)) {
-				((GuiButton)buttonList.get(0)).enabled = false;
-				((GuiButton)buttonList.get(1)).enabled = true;
-			} else if (oreName != "" && _itemSink.oreList.size() < 9) {
-				((GuiButton)buttonList.get(0)).enabled = true;
-				((GuiButton)buttonList.get(1)).enabled = false;
-			} else {
-				((GuiButton)buttonList.get(0)).enabled = false;
-				((GuiButton)buttonList.get(1)).enabled = false;
+			List<String> oreNames = getOreNames(tmpInv.getStackInSlot(0));
+			for(String name : oreNames) {
+				if(!unsunkNames.contains(name))
+					unsunkNames.add(name);
 			}
-		} else {
-			((GuiButton)buttonList.get(0)).enabled = false;
-			((GuiButton)buttonList.get(1)).enabled = false;
+			tmpInv.clearInventorySlotContents(0);
 		}
+
+		if(currentOffset > unsunkNames.size() - 2)
+			currentOffset = unsunkNames.size() - 2;
+		if(currentOffset < 0)
+			currentOffset = 0;
+
+		//draw unsunk list and highlight bar, handle clicks
+		BasicGuiHelper.drawRect(guiLeft + 26, guiTop + 5, guiLeft + 159, guiTop + 27, 0xff808080);
+		for(int i=0; i + currentOffset < unsunkNames.size() && i < 2; i++) {
+			if(27 <= pointerX && pointerX < 158 && 6 + (10 * i) <= pointerY && pointerY < 6 + (10 * (i + 1))) {
+				BasicGuiHelper.drawRect(guiLeft + 27, guiTop + 6 + (10 * i), guiLeft + 158, guiTop + 6 + (10 * (i + 1)), 0xffc0c0c0);
+			}
+			mc.fontRenderer.drawString(unsunkNames.get(currentOffset + i), guiLeft + 28, guiTop + 7 + (10 * i), 0x404040);
+			if(27 <= mouseX && mouseX < 158 && 6 + (10 * i) <= mouseY && mouseY < 6 + (10 * (i + 1))) {
+				mouseX = 0;
+				mouseY = 0;
+				if(_itemSink.oreList.size() < 9) {
+					String oreName = unsunkNames.get(currentOffset + i);
+					if(!_itemSink.oreList.contains(oreName)) {
+						_itemSink.oreList.add(oreName);
+						_itemSink.OreListChanged();
+					}
+					unsunkNames.remove(oreName);
+				}
+			}
+		}
+
+		//draw main list and highlight bar, handle clicks
 		BasicGuiHelper.drawRect(guiLeft + 5, guiTop + 30, guiLeft + 169, guiTop + 122, 0xff808080);
 		for(int i=0; i < _itemSink.oreList.size() && i < 9; i++) {
-			int pointerX = var2 - guiLeft;
-			int pointerY = var3 - guiTop;
 			if(6 <= pointerX && pointerX < 168 && 31 + (10 * i) <= pointerY && pointerY < 31 + (10 * (i + 1))) {
 				BasicGuiHelper.drawRect(guiLeft + 6, guiTop + 31 + (10 * i), guiLeft + 168, guiTop + 31 + (10 * (i + 1)), 0xffc0c0c0);
 			}
@@ -113,27 +127,23 @@ public class GuiOreDictItemSink extends ModuleBaseGui {
 			if(6 <= mouseX && mouseX < 168 && 31 + (10 * i) <= mouseY && mouseY < 31 + (10 * (i + 1))) {
 				mouseX = 0;
 				mouseY = 0;
-
 				String oreName = _itemSink.oreList.get(i);
-				if(!oreName.equals("")) {
-					ArrayList<ItemStack> items = OreDictionary.getOres(oreName);
-					if(items.size() > 0 && items.get(0) != null) {
-						ItemStack t = items.get(0).copy();
-						if(t.getItemDamage() == OreDictionary.WILDCARD_VALUE)
-							t.setItemDamage(0);
-						tmpInv.setInventorySlotContents(0, t);
-					} else {
-						tmpInv.clearInventorySlotContents(0);
-					}
-				}
+				if(!unsunkNames.contains(oreName))
+					unsunkNames.add(oreName);
+				_itemSink.oreList.remove(oreName);
+				_itemSink.OreListChanged();
 			}
 		}
 	}
 
-	private String getOreName(ItemStack s) {
-		int oreid = OreDictionary.getOreID(s);
-		if(oreid == -1)
-			return "";
-		return OreDictionary.getOreName(oreid);
+	private List<String> getOreNames(ItemStack s) {
+		int oreids[] = OreDictionary.getOreIDs(s);
+		List<String> oreNames = new ArrayList<String>(oreids.length);
+		for(int i=0; i < oreids.length; i++) {
+			String oreName = OreDictionary.getOreName(oreids[i]);
+			if(oreName != null && !oreName.equals("Unknown") && !oreNames.contains(oreName))
+				oreNames.add(oreName);
+		}
+		return oreNames;
 	}
 }

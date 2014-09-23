abe51f932305b4c5d622439b8f521abfd92e2f35
davboecki <dboeken@yahoo.de>
2014-09-23 18:01:13 +0200

    Allow LP to deal with recipe overlapping.
    Allows users do decide which version of for example a item they want to have. Either the TE version or the IC2 version.
    This also helps if there is a recipe overlapping another with a completely different outcome. With this the user can bypass that problem until it is fixed by the original mod author.

---
+++ common/logisticspipes/network/packets/block/CraftingSetType.java
@@ -0,0 +1,58 @@
+package logisticspipes.network.packets.block;
+
+import java.io.IOException;
+
+import logisticspipes.blocks.crafting.LogisticsCraftingTableTileEntity;
+import logisticspipes.network.LPDataInputStream;
+import logisticspipes.network.LPDataOutputStream;
+import logisticspipes.network.abstractpackets.CoordinatesPacket;
+import logisticspipes.network.abstractpackets.ModernPacket;
+import logisticspipes.pipes.PipeBlockRequestTable;
+import logisticspipes.pipes.basic.LogisticsTileGenericPipe;
+import logisticspipes.utils.item.ItemIdentifier;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+
+@Accessors(chain=true)
+public class CraftingSetType extends CoordinatesPacket {
+	
+	public CraftingSetType(int id) {
+		super(id);
+	}
+
+	@Getter
+	@Setter
+	private ItemIdentifier targetType;
+	
+	@Override
+	public void processPacket(EntityPlayer player) {
+		TileEntity table = this.getTile(player.getEntityWorld(), TileEntity.class);
+		if(table instanceof LogisticsCraftingTableTileEntity) {
+			((LogisticsCraftingTableTileEntity)table).targetType = targetType;
+			((LogisticsCraftingTableTileEntity)table).cacheRecipe();
+		} else if(table instanceof LogisticsTileGenericPipe && ((LogisticsTileGenericPipe)table).pipe instanceof PipeBlockRequestTable) {
+			((PipeBlockRequestTable)((LogisticsTileGenericPipe)table).pipe).targetType = targetType;
+			((PipeBlockRequestTable)((LogisticsTileGenericPipe)table).pipe).cacheRecipe();
+		}
+	}
+	
+	@Override
+	public ModernPacket template() {
+		return new CraftingSetType(getId());
+	}
+
+	@Override
+	public void writeData(LPDataOutputStream data) throws IOException {
+		super.writeData(data);
+		data.writeItemIdentifier(targetType);
+	}
+
+	@Override
+	public void readData(LPDataInputStream data) throws IOException {
+		super.readData(data);
+		targetType = data.readItemIdentifier();
+	}
+}

df42c8fcb48da49d5e01576fd13a495ca86a3796
Prototik <me@prok.pw>
2014-11-22 21:27:53 +0700

    Add special connection for new EnderIO's Dimensional Transceiver
    Conflicts:
    	common/logisticspipes/transport/PipeTransportLogistics.java

--- common/logisticspipes/interfaces/routing/ISpecialTileConnection.java
+++ common/logisticspipes/interfaces/routing/ISpecialTileConnection.java
@@ -1,14 +1,14 @@
 package logisticspipes.interfaces.routing;
 
-import java.util.List;
-
 import logisticspipes.logisticspipes.IRoutedItem;
 import net.minecraft.tileentity.TileEntity;
+
+import java.util.Collection;
 
 public interface ISpecialTileConnection {
 	public boolean init();
 	public boolean isType(TileEntity tile);
-	public List<TileEntity> getConnections(TileEntity tile);
+	public Collection<TileEntity> getConnections(TileEntity tile);
 	public boolean needsInformationTransition();
 	public void transmit(TileEntity tile, IRoutedItem arrivingItem);
 }
---
+++ dummy/crazypants/enderio/machine/transceiver/Channel.java
@@ -0,0 +1,5 @@
+package crazypants.enderio.machine.transceiver;
+
+public class Channel {
+
+}

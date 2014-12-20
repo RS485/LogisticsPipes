31fd5f1f1b84e115eae53f42ff68b77020b37df8
davboecki <dboeken@yahoo.de>
2014-12-20 18:01:42 +0100

    Update to OpenComputers 1.4 API. Close #519.

--- common/logisticspipes/proxy/opencomputers/asm/BaseWrapperClass.java
+++ common/logisticspipes/proxy/opencomputers/asm/BaseWrapperClass.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
-import li.cil.oc.api.network.Arguments;
-import li.cil.oc.api.network.Callback;
-import li.cil.oc.api.network.Context;
+import li.cil.oc.api.machine.Arguments;
+import li.cil.oc.api.machine.Callback;
+import li.cil.oc.api.machine.Context;
 import li.cil.oc.api.prefab.AbstractValue;
 import logisticspipes.LPConstants;
 import logisticspipes.LogisticsPipes;

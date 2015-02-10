914fd7682116358bf002d9ec4d10fe9602c4ee76
Bitterholz <simon.wienecke94@googlemail.com>
2015-02-10 14:44:15 +0100

    Update to BC 6.3.2

--- common/logisticspipes/asm/bc/ClassPipeTransportItemsHandler.java
+++ common/logisticspipes/asm/bc/ClassPipeTransportItemsHandler.java
@@ -48,10 +48,11 @@ public class ClassPipeTransportItemsHandler {
 		
 		String sumInjectItem = ASMHelper.getCheckSumForMethod(reader, "injectItem");
 		
-		if(!"956E67FF1103A53C970F22669CF70624DE3D4CF8".equals(sumInjectItem) && !"E7C1D1F202E00935B89B35E7F2A46B97E1FDC6F7".equals(sumInjectItem)) noChecksumMatch = true;
-
+		//if(!"956E67FF1103A53C970F22669CF70624DE3D4CF8".equals(sumInjectItem) && !"E7C1D1F202E00935B89B35E7F2A46B97E1FDC6F7".equals(sumInjectItem)) noChecksumMatch = true;
+		if(!"656CFA07E9337AC56FB6C1BA22EBBFAD604D83C0".equals(ASMHelper.getCheckSumForMethod(reader, "injectItem"))) noChecksumMatch = true;
+		
 		if(noChecksumMatch) {
-			System.out.println("injectItem: " + sumInjectItem);
+			System.out.println("injectItem:" + ASMHelper.getCheckSumForMethod(reader, "injectItem"));
 			new UnsupportedOperationException("This LP version isn't compatible with the installed BC version.").printStackTrace();
 			FMLCommonHandler.instance().exitJava(1, true);
 		}

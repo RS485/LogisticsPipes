c772d9bda8b1f7d5d1c6be0cc5ee840a76e69a9f
Benedikt Ziemons <root@theZorro266.com>
2015-05-20 21:11:35 +0200

    Added documentation and copyright to SimpleGraphics

--- common/logisticspipes/utils/gui/SimpleGraphics.java
+++ common/logisticspipes/utils/gui/SimpleGraphics.java
@@ -1,28 +1,58 @@
+/*
+ * Copyright (c) 2015  RS485
+ *
+ * "LogisticsPipes" is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * https://github.com/RS485/LogisticsPipes/blob/mc16/LICENSE.md
+ */
+
 package logisticspipes.utils.gui;
 
 import net.minecraft.client.gui.Gui;
 
+/**
+ * Utils class for simple drawing methods.
+ */
 public final class SimpleGraphics {
+
 	private SimpleGraphics() {
 	}
 
-	public static void drawHorizontalLine(int par1, int par2, int par3, int par4, int thickness) {
-		if(par2 < par1) {
-			int i1 = par1;
-			par1 = par2;
-			par2 = i1;
+	/**
+	 * Draws a horizontal line from x1 to x2.
+	 *
+	 * @param x1 the start coordinate
+	 * @param x2 the end coordinate
+	 * @param y the y-coordinate the line is on
+	 * @param color the color, which the line will have
+	 * @param thickness the thickness, which the line will have
+	 */
+	public static void drawHorizontalLine(int x1, int x2, int y, int color, int thickness) {
+		if (x2 < x1) {
+			int temp = x1;
+			x1 = x2;
+			x2 = temp;
 		}
 
-		Gui.drawRect(par1, par3, par2 + 1, par3 + thickness, par4);
+		Gui.drawRect(x1, y, x2 + 1, y + thickness, color);
 	}
 
-	public static void drawVerticalLine(int par1, int par2, int par3, int par4, int thickness) {
-		if(par3 < par2) {
-			int i1 = par2;
-			par2 = par3;
-			par3 = i1;
+	/**
+	 * Draws a vertical line from y1 to y2.
+	 *
+	 * @param x the x-coordinate the line is on
+	 * @param y1 the start coordinate
+	 * @param y2 the end coordinate
+	 * @param color the color, which the line will have
+	 * @param thickness the thickness, which the line will have
+	 */
+	public static void drawVerticalLine(int x, int y1, int y2, int color, int thickness) {
+		if (y2 < y1) {
+			int temp = y1;
+			y1 = y2;
+			y2 = temp;
 		}
 
-		Gui.drawRect(par1, par2 + 1, par1 + thickness, par3, par4);
+		Gui.drawRect(x, y1 + 1, x + thickness, y2, color);
 	}
 }

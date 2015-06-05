61f1d4c678a8ced5db193bc83f418c590716b16a
davboecki <dboeken@yahoo.de>
2015-06-06 01:17:39 +0200

    Remove GregTech DigitalChest compatibility again.

--- common/logisticspipes/proxy/SpecialInventoryHandlerManager.java
+++ common/logisticspipes/proxy/SpecialInventoryHandlerManager.java
@@ -4,7 +4,6 @@ import logisticspipes.proxy.specialinventoryhandler.AEInterfaceInventoryHandler;
 import logisticspipes.proxy.specialinventoryhandler.BarrelInventoryHandler;
 import logisticspipes.proxy.specialinventoryhandler.CrateInventoryHandler;
 import logisticspipes.proxy.specialinventoryhandler.DSUInventoryHandler;
-import logisticspipes.proxy.specialinventoryhandler.DigitalChestHandler;
 import logisticspipes.proxy.specialinventoryhandler.JABBAInventoryHandler;
 import logisticspipes.proxy.specialinventoryhandler.StorageDrawersInventoryHandler;
 
@@ -15,10 +14,6 @@ public class SpecialInventoryHandlerManager {
 	public static void load() {
 		if (Loader.isModLoaded("factorization")) {
 			SimpleServiceLocator.inventoryUtilFactory.registerHandler(new BarrelInventoryHandler());
-		}
-
-		if (Loader.isModLoaded("GregTech_Addon")) {
-			SimpleServiceLocator.inventoryUtilFactory.registerHandler(new DigitalChestHandler());
 		}
 
 		if (Loader.isModLoaded("betterstorage")) {

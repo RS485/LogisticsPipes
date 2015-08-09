d23f8dd0b48cfe9fd9e3b87a3fbc79f5d3604765
theZorro266 <root@theZorro266.com>
2015-08-09 16:58:19 +0200

    Add new coordinates classes

---
+++ common/logisticspipes/world/WorldCoordinatesWrapper.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2015  RS485
+ *
+ * "LogisticsPipes" is distributed under the terms of the Minecraft Mod Public
+ * License 1.0.1, or MMPL. Please check the contents of the license located in
+ * https://github.com/RS485/LogisticsPipes/blob/dev/LICENSE.md
+ *
+ * This file can instead be distributed under the license terms of the MIT license:
+ *
+ * Copyright (c) 2015  RS485
+ *
+ * This MIT license was reworded to only match this file. If you use the regular MIT license in your project, replace this copyright notice (this line and any lines below and NOT the copyright line above) with the lines from the original MIT license located here: http://opensource.org/licenses/MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this file and associated documentation files (the "Source Code"), to deal in the Source Code without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Source Code, and to permit persons to whom the Source Code is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Source Code, which also can be distributed under the MIT.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package logisticspipes.world;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import logisticspipes.LPConstants;
+import logisticspipes.LogisticsPipes;
+import logisticspipes.proxy.MainProxy;
+import logisticspipes.proxy.SimpleServiceLocator;
+import logisticspipes.routing.pathfinder.IPipeInformationProvider.ConnectionPipeType;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+import net.minecraftforge.common.util.ForgeDirection;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class WorldCoordinatesWrapper {
+
+	private World world;
+	private Coordinates coords;
+
+	public WorldCoordinatesWrapper(World world) {
+		setWorld(world);
+		setCoords(new Coordinates());
+	}
+
+	public WorldCoordinatesWrapper(World world, Coordinates coords) {
+		setWorld(world);
+		setCoords(coords);
+	}
+
+	public WorldCoordinatesWrapper(World world, int xCoord, int yCoord, int zCoord) {
+		setWorld(world);
+		setCoords(new Coordinates(xCoord, yCoord, zCoord));
+	}
+
+	public WorldCoordinatesWrapper(TileEntity tileEntity) {
+		this(tileEntity.getWorldObj(), tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord);
+	}
+
+	public void setWorld(World world) {
+		if (world == null) throw new NullPointerException("World must not be null");
+		this.world = world;
+	}
+
+	public void setCoords(Coordinates coords) {
+		if (coords == null) throw new NullPointerException("Coordinates must not be null");
+		this.coords = coords;
+	}
+
+	public Stream<AdjacentTileEntity> getAdjacentTileEntities() {
+		return Arrays.stream(ForgeDirection.VALID_DIRECTIONS).map(this::getAdjacentFromDirection).filter(tile -> tile != null);
+	}
+
+	public Stream<AdjacentTileEntity> getConnectedAdjacentTileEntities() {
+		TileEntity pipe = getTileEntity();
+		if (SimpleServiceLocator.pipeInformationManager.isNotAPipe(pipe)) {
+			LogisticsPipes.log.warn("The coordinates didn't hold a pipe at all", new Throwable("Stack trace"));
+			return Stream.empty();
+		}
+		return getAdjacentTileEntities().filter(adjacent -> MainProxy.checkPipesConnections(pipe, adjacent.adjTileEntity, adjacent.direction));
+	}
+
+	public Stream<AdjacentTileEntity> getConnectedAdjacentTileEntities(ConnectionPipeType pipeType) {
+		TileEntity pipe = getTileEntity();
+		if (!SimpleServiceLocator.pipeInformationManager.isPipe(pipe, true, pipeType)) {
+			if (LPConstants.DEBUG) {
+				LogisticsPipes.log.warn("The coordinates didn't hold the pipe type " + pipeType, new Throwable("Stack trace"));
+			}
+			return Stream.empty();
+		}
+		return getAdjacentTileEntities().filter(adjacent -> MainProxy.checkPipesConnections(pipe, adjacent.adjTileEntity, adjacent.direction));
+	}
+
+	public TileEntity getTileEntity() {
+		return world.getTileEntity(coords.getXCoord(), coords.getYCoord(), coords.getZCoord());
+	}
+
+	public AdjacentTileEntity getAdjacentFromDirection(ForgeDirection direction) {
+		Coordinates newCoords = new Coordinates(coords).add(direction);
+		return new AdjacentTileEntity(world.getTileEntity(newCoords.getXCoord(), newCoords.getYCoord(), newCoords.getZCoord()), direction);
+	}
+
+	@Data
+	@AllArgsConstructor
+	public static class AdjacentTileEntity {
+
+		public TileEntity adjTileEntity;
+		public ForgeDirection direction;
+	}
+}

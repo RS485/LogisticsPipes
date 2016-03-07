fb2f4f9f8037f5c73beb49e7d4b823821cc2caf7
theZorro266 <root@theZorro266.com>
2016-03-07 17:24:32 +0100

    Added input and output interfaces

---
+++ common/network/rs485/logisticspipes/util/LPDataInput.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2015  RS485
+ *
+ * "LogisticsPipes" is distributed under the terms of the Minecraft Mod Public
+ * License 1.0.1, or MMPL. Please check the contents of the license located in
+ * https://github.com/RS485/LogisticsPipes/blob/dev/LICENSE.md
+ *
+ * This file can instead be distributed under the license terms of the MIT license:
+ *
+ * Copyright (c) 2015  RS485
+ *
+ * This MIT license was reworded to only match this file. If you use the regular MIT license in your project, replace this copyright notice (this line and any lines below and NOT the copyright line above) with the lines from the original MIT license located here: http://opensource.org/licenses/MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this file and associated documentation files (the "Source Code"), to deal in the Source Code without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Source Code, and to permit persons to whom the Source Code is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Source Code, which also can be distributed under the MIT.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package network.rs485.logisticspipes.util;
+
+import java.io.IOException;
+import java.util.BitSet;
+import java.util.EnumSet;
+import java.util.List;
+
+import logisticspipes.network.IReadListObject;
+import logisticspipes.request.resources.IResource;
+import logisticspipes.routing.ExitRoute;
+import logisticspipes.routing.IRouter;
+import logisticspipes.routing.order.IOrderInfoProvider;
+import logisticspipes.routing.order.LinkedLogisticsOrderList;
+import logisticspipes.utils.item.ItemIdentifier;
+import logisticspipes.utils.item.ItemIdentifierStack;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+import net.minecraftforge.common.util.ForgeDirection;
+
+import io.netty.buffer.ByteBuf;
+import network.rs485.logisticspipes.world.DoubleCoordinates;
+
+public interface LPDataInput {
+
+	byte[] readLengthAndBytes();
+
+	byte readByte();
+
+	short readShort();
+
+	int readInt();
+
+	long readLong();
+
+	float readFloat();
+
+	double readDouble();
+
+	boolean readBoolean();
+
+	String readUTF();
+
+	ForgeDirection readForgeDirection();
+
+	ExitRoute readExitRoute(World world) throws IOException;
+
+	/**
+	 * @return ServerRouter or ClientRouter depending where we are
+	 */
+	IRouter readIRouter(World world);
+
+	DoubleCoordinates readLPPosition();
+
+	<T extends Enum<T>> EnumSet<T> readEnumSet(Class<T> clazz);
+
+	BitSet readBitSet();
+
+	NBTTagCompound readNBTTagCompound() throws IOException;
+
+	boolean[] readBooleanArray();
+
+	int[] readIntArray();
+
+	ItemIdentifier readItemIdentifier() throws IOException;
+
+	ItemIdentifierStack readItemIdentifierStack() throws IOException;
+
+	<T> List<T> readList(IReadListObject<T> handler) throws IOException;
+
+	IOrderInfoProvider readOrderInfo() throws IOException;
+
+	<T extends Enum<T>> T readEnum(Class<T> clazz);
+
+	LinkedLogisticsOrderList readLinkedLogisticsOrderList() throws IOException;
+
+	ByteBuf readByteBuf();
+
+	long[] readLongArray();
+
+	IResource readResource() throws IOException;
+
+	interface LPDataInputConsumer {
+
+		void accept(LPDataInput dataInput) throws IOException;
+	}
+}
---
+++ common/network/rs485/logisticspipes/util/LPDataOutput.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2015  RS485
+ *
+ * "LogisticsPipes" is distributed under the terms of the Minecraft Mod Public
+ * License 1.0.1, or MMPL. Please check the contents of the license located in
+ * https://github.com/RS485/LogisticsPipes/blob/dev/LICENSE.md
+ *
+ * This file can instead be distributed under the license terms of the MIT license:
+ *
+ * Copyright (c) 2015  RS485
+ *
+ * This MIT license was reworded to only match this file. If you use the regular MIT license in your project, replace this copyright notice (this line and any lines below and NOT the copyright line above) with the lines from the original MIT license located here: http://opensource.org/licenses/MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this file and associated documentation files (the "Source Code"), to deal in the Source Code without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Source Code, and to permit persons to whom the Source Code is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Source Code, which also can be distributed under the MIT.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package network.rs485.logisticspipes.util;
+
+import java.io.IOException;
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.EnumSet;
+
+import logisticspipes.network.IWriteListObject;
+import logisticspipes.request.resources.IResource;
+import logisticspipes.routing.ExitRoute;
+import logisticspipes.routing.IRouter;
+import logisticspipes.routing.order.IOrderInfoProvider;
+import logisticspipes.routing.order.LinkedLogisticsOrderList;
+import logisticspipes.utils.item.ItemIdentifier;
+import logisticspipes.utils.item.ItemIdentifierStack;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+import net.minecraftforge.common.util.ForgeDirection;
+
+import io.netty.buffer.ByteBuf;
+import network.rs485.logisticspipes.world.DoubleCoordinates;
+
+public interface LPDataOutput {
+
+	void writeByte(int b);
+
+	void writeByte(byte b);
+
+	void writeShort(int s);
+
+	void writeShort(short s);
+
+	void writeInt(int i);
+
+	void writeLong(long l);
+
+	void writeFloat(float f);
+
+	void writeDouble(double d);
+
+	void writeBoolean(boolean b);
+
+	/**
+	 * Uses UTF-8 and not UTF-16. This makes sense for most of the use cases.
+	 */
+	void writeUTF(String s);
+
+	void writeByteArray(byte[] data);
+
+	void writeLengthAndBytes(byte[] arr);
+
+	void writeByteBuf(ByteBuf buffer);
+
+	void writeIntArray(int[] arr);
+
+	void writeLongArray(long[] arr);
+
+	void writeBooleanArray(boolean[] arr);
+
+	void writeForgeDirection(ForgeDirection direction);
+
+	void writeExitRoute(ExitRoute route) throws IOException;
+
+	void writeIRouter(IRouter router);
+
+	void writeLPPosition(DoubleCoordinates pos);
+
+	<T extends Enum<T>> void writeEnumSet(EnumSet<T> types, Class<T> clazz);
+
+	void writeBitSet(BitSet bits);
+
+	void writeNBTTagCompound(NBTTagCompound tag) throws IOException;
+
+	void writeItemIdentifier(ItemIdentifier item) throws IOException;
+
+	void writeItemIdentifierStack(ItemIdentifierStack stack) throws IOException;
+
+	<T> void writeCollection(Collection<T> collection, IWriteListObject<T> handler) throws IOException;
+
+	void writeOrderInfo(IOrderInfoProvider order) throws IOException;
+
+	<T extends Enum<T>> void writeEnum(T obj);
+
+	void writeLinkedLogisticsOrderList(LinkedLogisticsOrderList orderList) throws IOException;
+
+	void writeResource(IResource res) throws IOException;
+
+	interface LPDataOutputConsumer {
+
+		void accept(LPDataOutput dataOutput) throws IOException;
+	}
+}

3b52d2d182afd9b3f8ced3a6b617a2b634cbe6ac
davboecki <dboeken@yahoo.de>
2016-04-27 19:02:36 +0200

    Fix #975

--- common/logisticspipes/proxy/MainProxy.java
+++ common/logisticspipes/proxy/MainProxy.java
@@ -228,15 +228,12 @@ public class MainProxy {
 		}
 	}
 
-	//XXX do we still need this?
 	private static boolean needsToBeCompressed(ModernPacket packet) {
-		/*
 		if(packet.getData() != null) {
 			if(packet.getData().length > 32767) {
 				return true; // Packet is to big
 			}
 		}
-		 */
 		return false;
 	}
 

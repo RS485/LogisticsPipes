ad3d150111ee1dcb4808f0f1407a2d7ec05a75de
theZorro266 <root@thezorro266.com>
2016-10-11 20:14:14 +0200

    Make LogisticsSolidBlock use new Block methods
    LogisticsSolidBlock is not switched to the new icon system, yet.

--- common/logisticspipes/blocks/LogisticsSolidBlock.java
+++ common/logisticspipes/blocks/LogisticsSolidBlock.java
@@ -1,21 +1,27 @@
 package logisticspipes.blocks;
 
-import net.minecraft.block.Block;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import logisticspipes.LPConstants;
+import static net.minecraft.util.EnumBlockRenderType.ENTITYBLOCK_ANIMATED;
+
 import logisticspipes.LogisticsPipes;
 import logisticspipes.blocks.crafting.LogisticsCraftingTableTileEntity;
 import logisticspipes.blocks.powertile.LogisticsIC2PowerProviderTileEntity;
@@ -43,17 +49,17 @@ public class LogisticsSolidBlock extends BlockContainer {
 	private static final TextureAtlasSprite[] newTextures = new TextureAtlasSprite[10];
 
 	@Override
-	public boolean isOpaqueCube() {
+	public boolean isOpaqueCube(IBlockState state) {
 		return false;
 	}
 
 	@Override
-	public boolean isSideSolid(IBlockAccess world, BlockPos pos, EnumFacing side) {
+	public boolean isSideSolid(IBlockState base_state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos, EnumFacing side) {
 		return true;
 	}
 
 	public LogisticsSolidBlock() {
-		super(Material.iron);
+		super(Material.IRON);
 		setCreativeTab(LogisticsPipes.LPCreativeTab);
 		setHardness(6.0F);
 	}
@@ -68,12 +74,13 @@ public class LogisticsSolidBlock extends BlockContainer {
 	}
 
 	@Override
-	public boolean onBlockActivated(World par1World, BlockPos pos, IBlockState state, EntityPlayer par5EntityPlayer, EnumFacing side, float par7, float par8, float par9) {
-		if (!par5EntityPlayer.isSneaking()) {
-			TileEntity tile = par1World.getTileEntity(pos);
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem,
+			EnumFacing side, float hitX, float hitY, float hitZ) {
+		if (!playerIn.isSneaking()) {
+			TileEntity tile = worldIn.getTileEntity(pos);
 			if (tile instanceof IGuiTileEntity) {
-				if (MainProxy.isServer(par5EntityPlayer.worldObj)) {
-					((IGuiTileEntity) tile).getGuiProvider().setTilePos(tile).open(par5EntityPlayer);
+				if (MainProxy.isServer(playerIn.worldObj)) {
+					((IGuiTileEntity) tile).getGuiProvider().setTilePos(tile).open(playerIn);
 				}
 				return true;
 			}
@@ -82,6 +89,7 @@ public class LogisticsSolidBlock extends BlockContainer {
 	}
 
 	@Override
+	@Nonnull
 	public IBlockState onBlockPlaced(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
 		IBlockState state = super.onBlockPlaced(world, pos, facing, hitX, hitY, hitZ, meta, placer);
 		TileEntity tile = world.getTileEntity(pos);
@@ -91,6 +99,8 @@ public class LogisticsSolidBlock extends BlockContainer {
 		if (tile instanceof IRotationProvider) {
 			double x = tile.getPos().getX() + 0.5 - placer.posX;
 			double z = tile.getPos().getZ() + 0.5 - placer.posZ;
+			// x and z is correct, atan2's first parameter is named y though
+			@SuppressWarnings("SuspiciousNameCombination")
 			double w = Math.atan2(x, z);
 			double halfPI = Math.PI / 2;
 			double halfhalfPI = halfPI / 2;
@@ -112,20 +122,21 @@ public class LogisticsSolidBlock extends BlockContainer {
 	}
 
 	@Override
-	public void breakBlock(World par1World, int par2, int par3, int par4, Block par5, int par6) {
-		TileEntity tile = par1World.getTileEntity(pos);
+	public void breakBlock(World worldIn, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
+		TileEntity tile = worldIn.getTileEntity(pos);
 		if (tile instanceof LogisticsSolderingTileEntity) {
 			((LogisticsSolderingTileEntity) tile).onBlockBreak();
 		}
 		if (tile instanceof LogisticsCraftingTableTileEntity) {
 			((LogisticsCraftingTableTileEntity) tile).onBlockBreak();
 		}
-		super.breakBlock(par1World, par2, par3, par4, par5, par6);
+		super.breakBlock(worldIn, pos, state);
 	}
 
 	@Override
-	public int getRenderType() {
-		return LPConstants.solidBlockModel;
+	@Nonnull
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return ENTITYBLOCK_ANIMATED;
 	}
 
 	@Override
@@ -134,7 +145,8 @@ public class LogisticsSolidBlock extends BlockContainer {
 	}
 
 	@Override
-	public TileEntity createNewTileEntity(World world, int metadata) {
+	@Nonnull
+	public TileEntity createNewTileEntity(@Nonnull World world, int metadata) {
 		switch (metadata) {
 			case SOLDERING_STATION:
 				return new LogisticsSolderingTileEntity();
@@ -152,7 +164,7 @@ public class LogisticsSolidBlock extends BlockContainer {
 			case LOGISTICS_IC2_POWERPROVIDER:
 				return new LogisticsIC2PowerProviderTileEntity();
 			default:
-				return null;
+				throw new IllegalArgumentException("Undefined meta");
 		}
 	}
 
@@ -196,7 +208,8 @@ public class LogisticsSolidBlock extends BlockContainer {
 		LogisticsSolidBlock.newTextures[2] = par1IIconRegister.registerIcon("logisticspipes:lpsolidblock/powerTexture"); // LOGISTICS_POWER_JUNCTION
 		LogisticsSolidBlock.newTextures[3] = par1IIconRegister.registerIcon("logisticspipes:lpsolidblock/securityTexture"); // LOGISTICS_SECURITY_STATION
 		LogisticsSolidBlock.newTextures[4] = par1IIconRegister.registerIcon("logisticspipes:lpsolidblock/craftingTexture"); // LOGISTICS_AUTOCRAFTING_TABLE
-		LogisticsSolidBlock.newTextures[5] = par1IIconRegister.registerIcon("logisticspipes:lpsolidblock/fuzzycraftingTexture"); // LOGISTICS_FUZZYCRAFTING_TABLE
+		LogisticsSolidBlock.newTextures[5] = par1IIconRegister
+				.registerIcon("logisticspipes:lpsolidblock/fuzzycraftingTexture"); // LOGISTICS_FUZZYCRAFTING_TABLE
 		LogisticsSolidBlock.newTextures[6] = par1IIconRegister.registerIcon("logisticspipes:lpsolidblock/statisticsTexture"); // LOGISTICS_STATISTICS_TABLE
 		LogisticsSolidBlock.newTextures[7] = par1IIconRegister.registerIcon("logisticspipes:lpsolidblock/powerRFTexture"); // LOGISTICS_RF_POWERPROVIDER
 		LogisticsSolidBlock.newTextures[8] = par1IIconRegister.registerIcon("logisticspipes:lpsolidblock/powerIC2Texture"); // LOGISTICS_IC2_POWERPROVIDER

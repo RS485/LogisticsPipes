33cf3e910a13ae510e0381da0f42448ddaf34eb3
davboecki <dboeken@yahoo.de>
2018-10-07 15:14:02 +0200

    Fix crash on server

--- common/logisticspipes/renderer/state/PipeSubRenderState.java
+++ common/logisticspipes/renderer/state/PipeSubRenderState.java
@@ -9,7 +9,6 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class PipeSubRenderState {
-	@SideOnly(Side.CLIENT)
 	public List<RenderEntry> cachedRenderer = null;
 	public GLRenderList renderList;
 	public boolean forceRenderOldPipe = false;

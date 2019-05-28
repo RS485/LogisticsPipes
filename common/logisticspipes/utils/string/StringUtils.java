34ad7f7870ab159b61e068e7ef7eaf300c2906e3
theZorro266 <ben@rs485.network>
2019-05-28 21:32:18 +0200

    Refactor calculation in StringUtils
    with simplification.

--- common/logisticspipes/utils/string/StringUtils.java
+++ common/logisticspipes/utils/string/StringUtils.java
@@ -111,11 +111,11 @@ public final class StringUtils {
 		} else if (stackSize < 100000) {
 			s = stackSize / 1000 + "K";
 		} else if (stackSize < 10000000) {
-			s = (int)stackSize / 1000000 + "M" + (stackSize - ((int)stackSize / 1000000) * 1000000) / 100000;
+			s = stackSize / 1000000 + "M" + (stackSize % 1000000) / 100000;
 		} else if (stackSize < 100000000) {
 			s = stackSize / 1000000 + "M";
 		} else {
-			s = (int)stackSize / 1000000000 + "G" + (stackSize - ((int)stackSize / 1000000000) * 1000000000) / 100000000;
+			s = stackSize / 1000000000 + "G" + (stackSize % 1000000000) / 100000000;
 		}
 		return s;
 	}

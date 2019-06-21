9dfc37ff70f1b59eceffef5174ad4f12026b2b8f
davboecki <dboeken@yahoo.de>
2019-06-21 20:47:21 +0200

    Add support for gregtech machines to insert into LP pipes

--- src/main/kotlin/network/rs485/logisticspipes/proxy/PipeInventoryConnectionChecker.kt
+++ src/main/kotlin/network/rs485/logisticspipes/proxy/PipeInventoryConnectionChecker.kt
@@ -39,6 +39,7 @@ package network.rs485.logisticspipes.proxy
 
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.tileentity.TileEntityHopper
+import net.minecraftforge.fml.common.Loader
 
 class PipeInventoryConnectionChecker {
     private val allowedConnectionClasses = mutableSetOf<Class<*>>()
@@ -46,6 +47,18 @@ class PipeInventoryConnectionChecker {
 
     init {
         allowedConnectionClasses.add(TileEntityHopper::class.java)
+        checkAndAddClass("gregtech", "gregtech.api.block.BlockStateTileEntity")
+    }
+
+    private fun checkAndAddClass(modId: String, className: String) {
+        if (Loader.isModLoaded(modId)) {
+            try {
+                val clazz = Class.forName(className)
+                if (clazz != null) {
+                    addSupportedClassType(clazz)
+                }
+            } catch (_: ClassNotFoundException) {}
+        }
     }
 
     fun addSupportedClassType(clazz: Class<*>) {

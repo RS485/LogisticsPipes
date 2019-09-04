d1ead1932901bf1fe856b008ea5fea1d660adb8e
tom5454 <tomecz.tomi@gmail.com>
2019-09-04 21:44:20 +0200

    Fix crash with FinalNBTTagCompound (#1369)
    * Fixing FinalNBTTagCompound
    
    Fixes crashes with items with NBT. (UnsupportedOperationException when constructing FinalNBTTagCompound)
    Fixes #1368.
    
    * Added NBTTagCompound.tagMap back
    
    * Revert "Added NBTTagCompound.tagMap back"
    
    This reverts commit d666ff569bc55411577a9a6706cdda0f4ba5353b.
    
    * Adding construction check to FinalNBTTagCompound
    
    * Formatting

--- common/logisticspipes/utils/FinalNBTTagCompound.java
+++ common/logisticspipes/utils/FinalNBTTagCompound.java
@@ -10,8 +10,13 @@ import net.minecraft.nbt.NBTTagCompound;
 
 public class FinalNBTTagCompound extends NBTTagCompound {
 
+	private boolean constructing;
+
 	public FinalNBTTagCompound(NBTTagCompound base) {
+		super();
+		constructing = true;
 		super.merge(base);
+		constructing = false;
 	}
 
 	@Nonnull
@@ -22,72 +27,85 @@ public class FinalNBTTagCompound extends NBTTagCompound {
 
 	@Override
 	public void setBoolean(@Nonnull String key, boolean value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setBoolean(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setByte(@Nonnull String key, byte value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setByte(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setByteArray(@Nonnull String key, @Nonnull byte[] value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setByteArray(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setDouble(@Nonnull String key, double value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setDouble(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setFloat(@Nonnull String key, float value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setFloat(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setIntArray(@Nonnull String key, @Nonnull int[] value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setIntArray(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setInteger(@Nonnull String key, int value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setInteger(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setLong(@Nonnull String key, long value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setLong(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setShort(@Nonnull String key, short value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setShort(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setString(@Nonnull String key, String value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setString(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setTag(@Nonnull String key, @Nonnull NBTBase value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setTag(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setUniqueId(String key, UUID value) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.setUniqueId(key, value);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void merge(NBTTagCompound other) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.merge(other);
+		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void removeTag(@Nonnull String key) {
-		throw new UnsupportedOperationException();
+		if (constructing) super.removeTag(key);
+		else throw new UnsupportedOperationException();
 	}
-
 }

f2fb2ba5b9350d47a9fdc8797ee4bdf474b5735a
therealfarfetchd <git@therealfarfetchd.dev>
2019-11-20 08:50:04 +0100

    Possible fix for #1394

--- common/logisticspipes/utils/FinalNBTTagCompound.java
+++ common/logisticspipes/utils/FinalNBTTagCompound.java
@@ -28,84 +28,71 @@ public class FinalNBTTagCompound extends NBTTagCompound {
 	@Override
 	public void setBoolean(@Nonnull String key, boolean value) {
 		if (constructing) super.setBoolean(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setByte(@Nonnull String key, byte value) {
 		if (constructing) super.setByte(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setByteArray(@Nonnull String key, @Nonnull byte[] value) {
 		if (constructing) super.setByteArray(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setDouble(@Nonnull String key, double value) {
 		if (constructing) super.setDouble(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setFloat(@Nonnull String key, float value) {
 		if (constructing) super.setFloat(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setIntArray(@Nonnull String key, @Nonnull int[] value) {
 		if (constructing) super.setIntArray(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setInteger(@Nonnull String key, int value) {
 		if (constructing) super.setInteger(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setLong(@Nonnull String key, long value) {
 		if (constructing) super.setLong(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setShort(@Nonnull String key, short value) {
 		if (constructing) super.setShort(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setString(@Nonnull String key, String value) {
 		if (constructing) super.setString(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setTag(@Nonnull String key, @Nonnull NBTBase value) {
 		if (constructing) super.setTag(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void setUniqueId(String key, UUID value) {
 		if (constructing) super.setUniqueId(key, value);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void merge(NBTTagCompound other) {
 		if (constructing) super.merge(other);
-		else throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void removeTag(@Nonnull String key) {
 		if (constructing) super.removeTag(key);
-		else throw new UnsupportedOperationException();
 	}
+
 }

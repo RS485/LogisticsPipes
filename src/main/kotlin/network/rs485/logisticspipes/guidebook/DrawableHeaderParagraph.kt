e859c62b0f8721d39c8dcedd65f256a5efcbb9f0
Bernardo Antunes <bernasss12@gmail.com>
2020-10-27 23:58:02 +0000

    Add a horizontal line to header paragraphs

--- src/main/kotlin/network/rs485/logisticspipes/guidebook/DrawableHeaderParagraph.kt
+++ src/main/kotlin/network/rs485/logisticspipes/guidebook/DrawableHeaderParagraph.kt
@@ -46,18 +46,15 @@ import network.rs485.logisticspipes.util.math.Rectangle
 data class DrawableHeaderParagraph(val drawables: List<DrawableWord>, val headerLevel: Int = 1) : IDrawableParagraph {
     override val area = Rectangle(0, 0)
     override var isHovered = true
-
-    // TODO add horizontal line
+    val horizontalLine = DrawableHorizontalLine(1)
 
     override fun draw(mouseX: Int, mouseY: Int, delta: Float, yOffset: Int, visibleArea: Rectangle) {
         super.draw(mouseX, mouseY, delta, yOffset, visibleArea)
         if (DEBUG_AREAS) area.translated(0, -yOffset).render(0.0f, 0.0f, 0.0f)
-        for (textToken in drawables.filter { visibleArea.translated(0, yOffset).overlaps(it.area) }) {
-            if (isHovered && textToken is Link) {
-                textToken.hovering(mouseX, mouseY, yOffset)
-            }
-            textToken.draw(mouseX, mouseY, delta, yOffset, visibleArea)
+        drawables.filter { visibleArea.overlaps(it.area.translated(0, -yOffset)) }.forEach { drawable ->
+            drawable.draw(mouseX, mouseY, delta, yOffset, visibleArea)
         }
+        if (visibleArea.overlaps(horizontalLine.area.translated(0, -yOffset))) horizontalLine.draw(mouseX, mouseY, delta, yOffset, visibleArea)
     }
 
     override fun setPos(x: Int, y: Int, maxWidth: Int): Int {
@@ -66,8 +63,9 @@ data class DrawableHeaderParagraph(val drawables: List<DrawableWord>, val header
     }
 
     override fun setChildrenPos(x: Int, y: Int, maxWidth: Int): Int {
-        val textHeight = splitInitialize(drawables, x, y, maxWidth)
-        area.setSize(maxWidth, textHeight)
+        var currentY = splitInitialize(drawables, x, y, maxWidth)
+        currentY += horizontalLine.setPos(x, y + currentY, maxWidth)
+        area.setSize(maxWidth, currentY)
         return area.height
     }
 }

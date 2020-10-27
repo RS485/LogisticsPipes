dca5583e3c63d287ba860e252ec59fa911eb88db
Bernardo Antunes <bernasss12@gmail.com>
2020-10-27 23:11:42 +0000

    Updated TabButton to use the MinecraftColor class

--- src/main/java/network/rs485/logisticspipes/gui/guidebook/GuiGuideBookTabButton.java
+++ src/main/java/network/rs485/logisticspipes/gui/guidebook/GuiGuideBookTabButton.java
@@ -45,11 +45,12 @@ import lombok.Getter;
 import lombok.Setter;
 import static network.rs485.logisticspipes.gui.guidebook.GuiGuideBook.GUI_BOOK_TEXTURE;
 
+import logisticspipes.utils.MinecraftColor;
+
 public class GuiGuideBookTabButton extends GuiButton {
 
-	private final int[] colors = { 0xE9ECEC, 0xF07613, 0xBD44B3, 0x3AAFD9, 0xF8C627, 0x70B919, 0xED8DAC, 0x3E4447, 0x8E8E86, 0x158991, 0x792AAC, 0x35399D, 0x724728, 0x546D1B, 0xA12722, 0x141519 };
-
 	public boolean isActive;
+
 	@Getter
 	@Setter
 	private SavedPage tab;
@@ -91,8 +92,7 @@ public class GuiGuideBookTabButton extends GuiButton {
 			drawY += 3;
 		}
 		this.hovered = mouseX > this.x && mouseX <= this.x + this.width && mouseY >= this.y - this.height && mouseY < this.y;
-		GuiGuideBook.drawStretchingSquare(x, drawY - drawHeight, x + width, drawY, 15, 40, 64, 40 + width, 64 + drawHeight, true, this.isActive ? 0xFFFFFF : colors[tab.getColor()]);
+		GuiGuideBook.drawStretchingSquare(x, drawY - drawHeight, x + width, drawY, 15, 40, 64, 40 + width, 64 + drawHeight, true, this.isActive ? 0xFFFFFF : MinecraftColor.values()[tab.getColor()].getColorCode());
 		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-
 	}
 }

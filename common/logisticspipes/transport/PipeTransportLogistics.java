9ab23f922304c67227fdd6e03289689266a6bbf5
Bernardo Antunes <bernasss12@gmail.com>
2022-09-12 15:35:33 +0100

    Fix insertArrivingItem handling empty stacks
    Add documentation to ITransactor.add

--- common/logisticspipes/transport/PipeTransportLogistics.java
+++ common/logisticspipes/transport/PipeTransportLogistics.java
@@ -567,9 +567,8 @@ public class PipeTransportLogistics {
 	private boolean insertArrivingItem(LPTravelingItemServer arrivingItem, TileEntity tile, EnumFacing insertion) {
 		ItemStack added = InventoryHelper.getTransactorFor(tile, insertion).add(arrivingItem.getItemIdentifierStack().makeNormalStack(), insertion, true);
 
-		arrivingItem.getItemIdentifierStack().lowerStackSize(added.getCount());
-
-		if (added.getCount() > 0) {
+		if (!added.isEmpty()) {
+			arrivingItem.getItemIdentifierStack().lowerStackSize(added.getCount());
 			arrivingItem.setBufferCounter(0);
 		}
 
@@ -579,7 +578,12 @@ public class PipeTransportLogistics {
 			// we have some leftovers, we are splitting the stack, we need to clone the info
 			info = info.clone();
 		}
-		info.getItem().setStackSize(added.getCount());
+		if(added.isEmpty()){
+			info.getItem().setStackSize(0);
+		} else {
+			info.getItem().setStackSize(added.getCount());
+		}
+
 		inventorySystemConnectorHook(info, tile);
 
 		// back to normal code, break if we've inserted everything, all items disposed of.
--- common/logisticspipes/utils/transactor/ITransactor.java
+++ common/logisticspipes/utils/transactor/ITransactor.java
@@ -7,6 +7,13 @@ import net.minecraft.util.EnumFacing;
 
 public interface ITransactor {
 
+	/**
+	 * Adds stack to the transactor
+	 * @param stack to add.
+	 * @param orientation side where transaction is being performed
+	 * @param doAdd whether to commit or simulate transaction
+	 * @return added stack.
+	 */
 	@Nonnull
 	ItemStack add(@Nonnull ItemStack stack, EnumFacing orientation, boolean doAdd);
 }

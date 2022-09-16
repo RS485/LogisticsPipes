3ebfe4da3f423ee0de16dd0bc37fe9119728baaf
theZorro266 <ben@rs485.network>
2022-09-16 19:31:59 +0200

    Add IC2C progress provider
    Fix #1667

--- common/logisticspipes/proxy/ic2/IC2ProgressProvider.java
+++ common/logisticspipes/proxy/ic2/IC2ProgressProvider.java
@@ -1,20 +1,64 @@
 package logisticspipes.proxy.ic2;
 
+import java.util.function.Function;
+import javax.annotation.Nullable;
+
 import net.minecraft.tileentity.TileEntity;
 
+import ic2.api.classic.tile.machine.IProgressMachine;
 import ic2.core.block.machine.tileentity.TileEntityStandardMachine;
 
+import logisticspipes.LogisticsPipes;
 import logisticspipes.proxy.interfaces.IGenericProgressProvider;
 
 public class IC2ProgressProvider implements IGenericProgressProvider {
 
+	@Nullable
+	private Class<?> progressProviderClass = null;
+	@Nullable
+	private Function<TileEntity, Byte> getProgressFunction = null;
+
+	public IC2ProgressProvider() {
+		try {
+			// support for IC2C progress
+			progressProviderClass = IProgressMachine.class;
+			getProgressFunction = this::getIC2CProgress;
+			return;
+		} catch (LinkageError error) {
+			if (LogisticsPipes.isDEBUG()) {
+				error.printStackTrace();
+			}
+		}
+		try {
+			progressProviderClass = TileEntityStandardMachine.class;
+			getProgressFunction = this::getIC2Progress;
+		} catch (LinkageError error) {
+			if (LogisticsPipes.isDEBUG()) {
+				error.printStackTrace();
+			}
+		}
+	}
+
 	@Override
 	public boolean isType(TileEntity tile) {
-		return tile instanceof TileEntityStandardMachine;
+		if (progressProviderClass == null) return false;
+		return progressProviderClass.isInstance(tile);
+	}
+
+	private byte getIC2CProgress(TileEntity tile) {
+		IProgressMachine progressMachine = (IProgressMachine) tile;
+		float normalizedProgress = progressMachine.getProgress() / progressMachine.getMaxProgress();
+		return (byte) Math.max(0, Math.min(normalizedProgress * 100, 100));
+	}
+
+	private byte getIC2Progress(TileEntity tile) {
+		TileEntityStandardMachine standardMachine = (TileEntityStandardMachine) tile;
+		return (byte) Math.max(0, Math.min(standardMachine.getProgress() * 100, 100));
 	}
 
 	@Override
 	public byte getProgress(TileEntity tile) {
-		return (byte) Math.max(0, Math.min(((TileEntityStandardMachine) tile).getProgress() * 100, 100));
+		if (getProgressFunction == null) return 0;
+		return getProgressFunction.apply(tile);
 	}
 }
---
+++ dummy/src/main/java/ic2/api/classic/tile/machine/IProgressMachine.java
@@ -0,0 +1,6 @@
+package ic2.api.classic.tile.machine;
+
+public interface IProgressMachine {
+	float getProgress();
+	float getMaxProgress();
+}

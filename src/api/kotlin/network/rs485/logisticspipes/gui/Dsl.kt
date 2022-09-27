b80c03b85384c8d948ba690ad15e42692c52e609
theZorro266 <ben@rs485.network>
2022-09-28 00:23:25 +0200

    Add GUI Dsl prototype

---
+++ src/api/kotlin/network/rs485/logisticspipes/gui/Dsl.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2022  RS485
+ *
+ * "LogisticsPipes" is distributed under the terms of the Minecraft Mod Public
+ * License 1.0.1, or MMPL. Please check the contents of the license located in
+ * https://github.com/RS485/LogisticsPipes/blob/dev/LICENSE.md
+ *
+ * This file can instead be distributed under the license terms of the
+ * MIT license:
+ *
+ * Copyright (c) 2022  RS485
+ *
+ * This MIT license was reworded to only match this file. If you use the regular
+ * MIT license in your project, replace this copyright notice (this line and any
+ * lines below and NOT the copyright line above) with the lines from the original
+ * MIT license located here: http://opensource.org/licenses/MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this file and associated documentation files (the "Source Code"), to deal in
+ * the Source Code without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Source Code, and to permit persons to whom the Source Code is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Source Code, which also can be
+ * distributed under the MIT.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package network.rs485.logisticspipes.gui
+
+import network.rs485.logisticspipes.property.Property
+import network.rs485.logisticspipes.property.PropertyLayer
+
+@DslMarker
+annotation class GuiComponentMarker
+
+@GuiComponentMarker
+sealed class GuiComponent {
+    enum class UndefinedId {
+        UNDEFINED
+    }
+
+    var id: Enum<*> = UndefinedId.UNDEFINED
+    val children = arrayListOf<GuiComponent>()
+
+    protected fun <T : GuiComponent> initComponent(component: T, init: T.() -> Unit): T {
+        component.apply(init)
+        children.add(component)
+        return component
+    }
+}
+
+class WidgetContainer : GuiComponent() {
+    fun <V : Any, P : Property<V>> label(init: PropertyLabel<V, P>.() -> Unit) = initComponent(PropertyLabel(), init)
+    fun button(init: Button.() -> Unit) = initComponent(Button(), init)
+}
+
+open class Label : GuiComponent() {
+    var text: String = ""
+}
+
+class PropertyLabel<V : Any, P : Property<V>> : Label() {
+    lateinit var propertyLayer: PropertyLayer
+    lateinit var property: P
+    var propertyToText: (V) -> String = Any::toString
+}
+
+open class Button : GuiComponent() {
+    var text: String = ""
+    var action: () -> Unit = {}
+}
+
+fun widgetContainer(init: WidgetContainer.() -> Unit): WidgetContainer =
+    WidgetContainer().apply(init)

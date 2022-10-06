c3e3ed304579b4be13442f48213f7571a0a4ee2d
theZorro266 <ben@rs485.network>
2022-10-06 19:06:09 +0200

    Use properties in GuiOreDictItemSink
    Fix #1609

--- common/logisticspipes/gui/modules/GuiOreDictItemSink.java
+++ common/logisticspipes/gui/modules/GuiOreDictItemSink.java
@@ -12,27 +12,36 @@ import net.minecraft.item.ItemStack;
 
 import net.minecraftforge.oredict.OreDictionary;
 
+import kotlin.Unit;
+
 import logisticspipes.modules.ModuleOreDictItemSink;
+import logisticspipes.network.packets.module.ModulePropertiesUpdate;
+import logisticspipes.proxy.MainProxy;
 import logisticspipes.utils.Color;
 import logisticspipes.utils.gui.DummyContainer;
 import logisticspipes.utils.gui.GuiGraphics;
 import logisticspipes.utils.gui.SmallGuiButton;
 import logisticspipes.utils.item.ItemIdentifierInventory;
+import network.rs485.logisticspipes.property.PropertyLayer;
+import network.rs485.logisticspipes.property.StringListProperty;
 
 public class GuiOreDictItemSink extends ModuleBaseGui {
 
 	private final ItemIdentifierInventory tmpInv;
+	private final PropertyLayer propertyLayer;
+	private final PropertyLayer.PropertyOverlay<List<String>, StringListProperty> oreListOverlay;
 
-	// FIXME please…
-	private final ArrayList<String> guiLocalOreList;
-	private final List<String> unsunkNames = new ArrayList<>();
 	private int mouseX = 0;
 	private int mouseY = 0;
+
+	private final List<String> unsunkNames = new ArrayList<>();
 	private int currentOffset = 0;
 
-	public GuiOreDictItemSink(IInventory playerInventory, ModuleOreDictItemSink moduleIn) {
-		super(null, moduleIn);
-		guiLocalOreList = new ArrayList<>(moduleIn.oreList);
+	public GuiOreDictItemSink(IInventory playerInventory, ModuleOreDictItemSink oreDictModule) {
+		super(null, oreDictModule);
+
+		propertyLayer = new PropertyLayer(oreDictModule.getProperties());
+		oreListOverlay = propertyLayer.overlay(oreDictModule.oreList);
 
 		tmpInv = new ItemIdentifierInventory(1, "Analyse Slot", 1);
 
@@ -54,6 +63,16 @@ public class GuiOreDictItemSink extends ModuleBaseGui {
 		buttonList.add(new SmallGuiButton(1, guiLeft + 159, guiTop + 17, 10, 10, ""));
 		buttonList.get(0).enabled = true;
 		buttonList.get(1).enabled = true;
+	}
+
+	@Override
+	public void onGuiClosed() {
+		super.onGuiClosed();
+		propertyLayer.unregister();
+		if (this.mc.player != null && !propertyLayer.getProperties().isEmpty()) {
+			// send update to server, when there are changed properties
+			MainProxy.sendPacketToServer(ModulePropertiesUpdate.fromPropertyHolder(propertyLayer).setModulePos(module));
+		}
 	}
 
 	@Override
@@ -101,43 +120,67 @@ public class GuiOreDictItemSink extends ModuleBaseGui {
 
 		//draw unsunk list and highlight bar, handle clicks
 		Gui.drawRect(guiLeft + 26, guiTop + 5, guiLeft + 159, guiTop + 27, Color.DARK_GREY.getValue());
-		for (int i = 0; i + currentOffset < unsunkNames.size() && i < 2; i++) {
-			if (27 <= pointerX && pointerX < 158 && 6 + (10 * i) <= pointerY && pointerY < 6 + (10 * (i + 1))) {
-				Gui.drawRect(guiLeft + 27, guiTop + 6 + (10 * i), guiLeft + 158, guiTop + 6 + (10 * (i + 1)),
-						Color.LIGHT_GREY.getValue());
-			}
-			mc.fontRenderer
-					.drawString(unsunkNames.get(currentOffset + i), guiLeft + 28, guiTop + 7 + (10 * i), 0x404040);
-			if (27 <= mouseX && mouseX < 158 && 6 + (10 * i) <= mouseY && mouseY < 6 + (10 * (i + 1))) {
-				mouseX = 0;
-				mouseY = 0;
-				if (guiLocalOreList.size() < 9) {
-					String oreName = unsunkNames.get(currentOffset + i);
-					if (!guiLocalOreList.contains(oreName)) {
-						guiLocalOreList.add(oreName);
+		final ArrayList<String> oresToAdd = oreListOverlay.read(oreList -> {
+			ArrayList<String> oresToAddInner = new ArrayList<>();
+			for (int i = 0; i + currentOffset < unsunkNames.size() && i < 2; i++) {
+				if (27 <= pointerX && pointerX < 158 && 6 + (10 * i) <= pointerY && pointerY < 6 + (10 * (i + 1))) {
+					Gui.drawRect(guiLeft + 27, guiTop + 6 + (10 * i), guiLeft + 158, guiTop + 6 + (10 * (i + 1)), Color.LIGHT_GREY.getValue());
+				}
+				mc.fontRenderer.drawString(unsunkNames.get(currentOffset + i), guiLeft + 28, guiTop + 7 + (10 * i), 0x404040);
+				if (27 <= mouseX && mouseX < 158 && 6 + (10 * i) <= mouseY && mouseY < 6 + (10 * (i + 1))) {
+					mouseX = 0;
+					mouseY = 0;
+					if (oreList.size() < 9) {
+						String oreName = unsunkNames.get(currentOffset + i);
+						if (!oreList.contains(oreName)) {
+							oresToAddInner.add(oreName);
+						}
+						unsunkNames.remove(oreName);
 					}
-					unsunkNames.remove(oreName);
 				}
 			}
+			return oresToAddInner;
+		});
+		if (!oresToAdd.isEmpty()) {
+			oreListOverlay.write(oreList -> {
+				for (String oreName : oresToAdd) {
+					if (!oreList.contains(oreName)) {
+						oreList.add(oreName);
+					}
+				}
+				return Unit.INSTANCE;
+			});
 		}
 
 		//draw main list and highlight bar, handle clicks
 		Gui.drawRect(guiLeft + 5, guiTop + 30, guiLeft + 169, guiTop + 122, Color.DARK_GREY.getValue());
-		for (int i = 0; i < guiLocalOreList.size() && i < 9; i++) {
-			if (6 <= pointerX && pointerX < 168 && 31 + (10 * i) <= pointerY && pointerY < 31 + (10 * (i + 1))) {
-				Gui.drawRect(guiLeft + 6, guiTop + 31 + (10 * i), guiLeft + 168, guiTop + 31 + (10 * (i + 1)),
-						Color.LIGHT_GREY.getValue());
-			}
-			mc.fontRenderer.drawString(guiLocalOreList.get(i), guiLeft + 7, guiTop + 32 + (10 * i), 0x404040);
-			if (6 <= mouseX && mouseX < 168 && 31 + (10 * i) <= mouseY && mouseY < 31 + (10 * (i + 1))) {
-				mouseX = 0;
-				mouseY = 0;
-				String oreName = guiLocalOreList.get(i);
-				if (!unsunkNames.contains(oreName)) {
-					unsunkNames.add(oreName);
+		final ArrayList<String> oresToRemove = oreListOverlay.read(oreList -> {
+			ArrayList<String> oresToRemoveInner = new ArrayList<>();
+			for (int i = 0; i < oreList.size() && i < 9; i++) {
+				if (6 <= pointerX && pointerX < 168 && 31 + (10 * i) <= pointerY && pointerY < 31 + (10 * (i + 1))) {
+					Gui.drawRect(guiLeft + 6, guiTop + 31 + (10 * i), guiLeft + 168, guiTop + 31 + (10 * (i + 1)), Color.LIGHT_GREY.getValue());
 				}
-				guiLocalOreList.remove(oreName);
+				mc.fontRenderer.drawString(oreList.get(i), guiLeft + 7, guiTop + 32 + (10 * i), 0x404040);
+				if (6 <= mouseX && mouseX < 168 && 31 + (10 * i) <= mouseY && mouseY < 31 + (10 * (i + 1))) {
+					mouseX = 0;
+					mouseY = 0;
+					String oreName = oreList.get(i);
+					if (!unsunkNames.contains(oreName)) {
+						unsunkNames.add(oreName);
+					}
+					oresToRemoveInner.add(oreName);
+				}
 			}
+			return oresToRemoveInner;
+		});
+
+		if (!oresToRemove.isEmpty()) {
+			oreListOverlay.write(oreList -> {
+				for (String oreName : oresToRemove) {
+					oreList.remove(oreName);
+				}
+				return Unit.INSTANCE;
+			});
 		}
 	}
 
